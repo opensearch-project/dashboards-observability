92b55e77c1a9b9ae90761059fa48710659fefda3
Eric Wei <menwe@amazon.com>
2023-02-02 16:25:54 -0800

    markdown doesnt care if there's data
    Signed-off-by: Eric Wei <menwe@amazon.com>

--- public/components/visualizations/visualization.tsx
+++ public/components/visualizations/visualization.tsx
@@ -7,13 +7,16 @@ import React from 'react';
 import { isEmpty } from 'lodash';
 import { VisualizationChart } from './visualization_chart';
 import { VisCanvassPlaceholder } from '../event_analytics/explorer/visualizations/shared_components';
+import { IVisualizationContainerPropsVis } from '../../../common/types/explorer';
 
-interface IVisualizationProps {}
-
-export const Visualization = ({ visualizations }: IVisualizationProps) => {
+export const Visualization = ({
+  visualizations,
+}: {
+  visualizations: IVisualizationContainerPropsVis;
+}) => {
   return (
     <>
-      {!isEmpty(visualizations?.data?.rawVizData?.data) ? (
+      {!isEmpty(visualizations?.data?.rawVizData?.data) || visualizations?.vis?.id === 'text' ? (
         <VisualizationChart visualizations={visualizations} />
       ) : (
         <VisCanvassPlaceholder message={'No data found'} icon={visualizations?.vis?.icontype} />
