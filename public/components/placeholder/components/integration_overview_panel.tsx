5f7d2366355f4329f1d17e46c7349153a24188bb
Derek Ho <dxho@amazon.com>
2023-03-10 12:11:09 -0500

    fix margins
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- public/components/placeholder/components/integration_overview_panel.tsx
+++ public/components/placeholder/components/integration_overview_panel.tsx
@@ -9,21 +9,60 @@ import {
   EuiTitle,
   EuiFlexItem,
   EuiText,
+  EuiPageContentHeaderSection,
 } from '@elastic/eui';
 import React from 'react';
 
+const pageStyles: CSS.Properties = {
+  width: '80%',
+};
+
 export function IntegrationOverview(props: { appId; link }) {
   return (
-    <EuiPageHeader>
-      <EuiPageHeaderSection>
-        <EuiFlexGroup>
-          <EuiTitle data-test-subj="eventHomePageTitle" size="l">
-            <EuiLink href={props.link} external={true} target="blank">
-              {props.appId}
-            </EuiLink>
-          </EuiTitle>
-          <EuiButton>Add</EuiButton>
-        </EuiFlexGroup>
+    <EuiPageHeader style={{ justifyContent: 'center' }}>
+      <EuiPageHeaderSection style={pageStyles}>
+        {/* <EuiPageContentHeader>
+              <EuiPageContentHeaderSection>
+                <EuiTitle data-test-subj="applicationHomePageTitle" size="s">
+                  <h3>
+                    Applications<span className="panel-header-count"> ({applications.length})</span>
+                  </h3>
+                </EuiTitle>
+              </EuiPageContentHeaderSection>
+              <EuiPageContentHeaderSection>
+                <EuiFlexGroup gutterSize="s">
+                  <EuiFlexItem>
+                    <EuiPopover
+                      panelPaddingSize="none"
+                      button={popoverButton}
+                      isOpen={isActionsPopoverOpen}
+                      closePopover={() => setIsActionsPopoverOpen(false)}
+                    >
+                      <EuiContextMenuPanel items={popoverItems} />
+                    </EuiPopover>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiButton fill href="#/application_analytics/create">
+                      {createButtonText}
+                    </EuiButton>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiPageContentHeaderSection>
+            </EuiPageContentHeader> */}
+        <EuiPageContentHeaderSection>
+          <EuiFlexGroup gutterSize="xs">
+            <EuiFlexItem>
+              <EuiTitle data-test-subj="eventHomePageTitle" size="l">
+                <EuiLink href={props.link} external={true} target="blank">
+                  {props.appId}
+                </EuiLink>
+              </EuiTitle>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiButton size="s">Add</EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPageContentHeaderSection>
         <EuiSpacer />
         <EuiFlexGroup>
           <EuiFlexItem>
