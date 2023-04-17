f421b9c9f0169d9779e0cbd09f6d8ba01d91fa5e
Joshua Li <joshuali925@gmail.com>
2023-04-17 16:15:05 -0700

    Support new notebooks URL for reporting (#373)
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- public/components/notebooks/components/helpers/__tests__/legacy_route_helpers.test.ts
+++ public/components/notebooks/components/helpers/__tests__/legacy_route_helpers.test.ts
@@ -26,10 +26,10 @@ describe('Test legacy route helpers', () => {
       },
     ] as Location[];
     const expected = [
-      '/app/observability-dashboards#/notebooks/GQ5icXwBJCegTOBKO4Um',
-      '/app/observability-dashboards#/notebooks/clPiPXwBEM7l9gC0xTpA?view=view_both',
-      `/testBasePath/app/observability-dashboards#/notebooks/GQ5icXwBJCegTOBKO4Um?_g=(time:(from:'2021-10-15T20:25:09.556Z',to:'2021-10-15T20:55:09.556Z'))&view=output_only&security_tenant=global`,
-    ] as RedirectProps['to'][];
+      '/app/observability-notebooks#/GQ5icXwBJCegTOBKO4Um',
+      '/app/observability-notebooks#/clPiPXwBEM7l9gC0xTpA?view=view_both',
+      `/testBasePath/app/observability-notebooks#/GQ5icXwBJCegTOBKO4Um?_g=(time:(from:'2021-10-15T20:25:09.556Z',to:'2021-10-15T20:55:09.556Z'))&view=output_only&security_tenant=global`,
+    ] as Array<RedirectProps['to']>;
     expect(locations.map((location) => convertLegacyNotebooksUrl(location))).toEqual(expected);
   });
 });
