c1b63a23920efda9b5479dcedac8c0f57b0f2412
Peter Fitzgibbons <pjfitz@amazon.com>
2023-04-17 16:34:10 -0700

    Merge latest changes from main
    Signed-off-by: Peter Fitzgibbons <pjfitz@amazon.com>

--- public/components/custom_panels/home.tsx
+++ public/components/custom_panels/home.tsx
@@ -31,9 +31,6 @@ import PPLService from '../../services/requests/ppl';
 import { CustomPanelTable } from './custom_panel_table';
 import { CustomPanelView } from './custom_panel_view';
 import { CustomPanelViewSO } from './custom_panel_view_so';
-import { isNameValid } from './helpers/utils';
-import { CustomPanelViewSO } from './custom_panel_view_so';
-import { coreRefs } from '../../framework/core_refs';
 import { deletePanel, fetchPanels, uuidRx } from './redux/panel_slice';
 
 // import { ObjectFetcher } from '../common/objectFetcher';
