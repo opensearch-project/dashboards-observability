53836167495e2e31299cddda3a0b7797be8bd139
Joshua Li <joshuali925@gmail.com>
2023-04-18 12:25:39 -0700

    Update jest tests and snapshots for panels with redux (#381)
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- public/components/event_analytics/explorer/save_panel/__tests__/save_panel.test.tsx
+++ public/components/event_analytics/explorer/save_panel/__tests__/save_panel.test.tsx
@@ -3,42 +3,56 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { applyMiddleware, createStore } from '@reduxjs/toolkit';
+import { waitFor } from '@testing-library/react';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { waitFor } from '@testing-library/react';
-import { SavePanel } from '../save_panel';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { SELECTED_PANELS_OPTIONS } from '../../../../../../test/event_analytics_constants';
-import SavedObjects from '../../../../../services/saved_objects/event_analytics/saved_objects';
 import httpClientMock from '../../../../../../test/__mocks__/httpClientMock';
+import { coreRefs } from '../../../../../framework/core_refs';
+import { rootReducer } from '../../../../../framework/redux/reducers';
+import SavedObjects from '../../../../../services/saved_objects/event_analytics/saved_objects';
+import { SavePanel } from '../save_panel';
 
 describe('Saved query table component', () => {
   configure({ adapter: new Adapter() });
+  const store = createStore(rootReducer, applyMiddleware(thunk));
+  coreRefs.savedObjectsClient.find = jest.fn(() =>
+    Promise.resolve({
+      savedObjects: [],
+      then: () => Promise.resolve(),
+    })
+  );
 
   it('Renders saved query table', async () => {
     const handleNameChange = jest.fn();
     const handleOptionChange = jest.fn();
     const setMetricLabel = jest.fn();
     const savedObjects = new SavedObjects(httpClientMock);
-    
+
     const wrapper = mount(
-      <SavePanel
-        selectedOptions={SELECTED_PANELS_OPTIONS}
-        handleNameChange={handleNameChange}
-        handleOptionChange={handleOptionChange}
-        savedObjects={savedObjects}
-        savePanelName={'Count by depature'}
-        showOptionList={true}
-        curVisId={'line'}
-        spanValue={false}
-        setSubType={'metric'}
-        setMetricMeasure={'hours (h)'}
-        setMetricLabel={setMetricLabel} 
-        metricMeasure={'hours (h)'} 
-        metricChecked={true}        
-      />
+      <Provider store={store}>
+        <SavePanel
+          selectedOptions={SELECTED_PANELS_OPTIONS}
+          handleNameChange={handleNameChange}
+          handleOptionChange={handleOptionChange}
+          savedObjects={savedObjects}
+          savePanelName={'Count by depature'}
+          showOptionList={true}
+          curVisId={'line'}
+          spanValue={false}
+          setSubType={'metric'}
+          setMetricMeasure={'hours (h)'}
+          setMetricLabel={setMetricLabel}
+          metricMeasure={'hours (h)'}
+          metricChecked={true}
+        />
+      </Provider>
     );
-    
+
     wrapper.update();
 
     await waitFor(() => {
