c74385b56617a28c7b374162fc63ff2e7625c98d
Peter Fitzgibbons <pjfitz@amazon.com>
2023-04-19 13:36:46 -0700

    Fixes * Panel View (legacy)   - Duplicate   - Rename
    Signed-off-by: Peter Fitzgibbons <pjfitz@amazon.com>

--- public/components/custom_panels/custom_panel_table.tsx
+++ public/components/custom_panels/custom_panel_table.tsx
@@ -222,7 +222,7 @@ export const CustomPanelTable = ({
         'Duplicate Dashboard',
         'Cancel',
         'Duplicate',
-        selectedCustomPanels[0].title + ' (copy)',
+        selectedCustomPanels[0].title + ' (copy)x',
         CREATE_PANEL_MESSAGE
       )
     );
