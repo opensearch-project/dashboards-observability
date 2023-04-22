55d126b6e0ea0956e8043d53d52aaad92a820d9f
Kawika Avilla <kavilla414@gmail.com>
2023-04-22 03:52:45 +0000

    [Dashboard listing] update edit url and view url
    Update to include BASE_URL if basePath is defined. Since create url is just
    a direct nav and needs the basePath when passing props to the provider, the source
    code no longer appends the basePath to keep these URLs consistent.
    
    A feature should consolidate the basePaths in the dashboard listing service so that
    createUrl also adds the base path. At which point we no longer need the base path.
    
    Also removed some rogue URLs.
    
    Core PR:
    https://github.com/opensearch-project/OpenSearch-Dashboards/pull/3922
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- public/components/custom_panels/custom_panel_view_so.tsx
+++ public/components/custom_panels/custom_panel_view_so.tsx
@@ -255,7 +255,6 @@ export const CustomPanelViewSO = (props: CustomPanelViewProps) => {
   }, [panel]);
 
   const cancelEdit = () => {
-    console.log('cancelEdits');
     dispatch(fetchPanel(panelId));
     setIsEditing(false);
   };
