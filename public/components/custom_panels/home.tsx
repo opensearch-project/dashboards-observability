ff221207a199a8bb0c825d6460d399d2224d5e4f
Shenoy Pratik <sgguruda@amazon.com>
2023-04-28 10:03:53 -0700

    Add Toasts to Observability Dashboards  (#435)
    * Fixes
    * Panel View (legacy)
      - Duplicate
      - Rename
    
    Signed-off-by: Peter Fitzgibbons <pjfitz@amazon.com>
    
    * Toasts use hook from useOpenSearchDashboards context provider
    
    Signed-off-by: Peter Fitzgibbons <pjfitz@amazon.com>
    
    * Testing for CustomPanel Toast
    
    Signed-off-by: Peter Fitzgibbons <pjfitz@amazon.com>
    
    * update catches from comments, minor code cleaning
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * update tests
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * remove unused redux slice
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * revert cypress changes
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * add toasts to SOflyout
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * fix messaging for multiple delete
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix up toast and error handling for create and delete flows
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix up clone
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix rename in table
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix rename in custom panel so view
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix up panel toasts
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix up for flyout
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * code cleanup
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * finish merge
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * fix up PR comments
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    ---------
    
    Signed-off-by: Peter Fitzgibbons <pjfitz@amazon.com>
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    Signed-off-by: Derek Ho <dxho@amazon.com>
    Co-authored-by: Peter Fitzgibbons <pjfitz@amazon.com>
    Co-authored-by: Derek Ho <dxho@amazon.com>
    (cherry picked from commit a9d1d37035f41d5e81f521f16dd4ebcfaf478398)
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- public/components/custom_panels/home.tsx
+++ public/components/custom_panels/home.tsx
@@ -3,10 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { EuiBreadcrumb, EuiGlobalToastList, ShortDate } from '@elastic/eui';
-import { Toast } from '@elastic/eui/src/components/toast/global_toast_list';
-import React, { ReactChild, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { EuiBreadcrumb, ShortDate, htmlIdGenerator } from '@elastic/eui';
+import React, { useState } from 'react';
+import { useDispatch, batch } from 'react-redux';
 // eslint-disable-next-line @osd/eslint/module_migration
 import { StaticContext } from 'react-router';
 import { HashRouter, Route, RouteComponentProps, Switch } from 'react-router-dom';
@@ -28,15 +27,15 @@ import PPLService from '../../services/requests/ppl';
 import { CustomPanelTable } from './custom_panel_table';
 import { CustomPanelView } from './custom_panel_view';
 import { CustomPanelViewSO } from './custom_panel_view_so';
-import {
-  createPanel,
-  createPanelSample,
-  createPanelWithVizs,
-  deletePanel,
-  fetchPanels,
-  newPanelTemplate,
-  uuidRx,
-} from './redux/panel_slice';
+import { fetchPanels, uuidRx } from './redux/panel_slice';
+import { REDIRECT_TAB, TAB_CREATED_TYPE, TAB_ID_TXT_PFX } from '../../../common/constants/explorer';
+import { init as initFields } from '../event_analytics/redux/slices/field_slice';
+import { init as initPatterns } from '../event_analytics/redux/slices/patterns_slice';
+import { init as initQueryResult } from '../event_analytics/redux/slices/query_result_slice';
+import { changeQuery, init as initQuery } from '../event_analytics/redux/slices/query_slice';
+import { addTab, setSelectedQueryTab } from '../event_analytics/redux/slices/query_tab_slice';
+import { useToast } from '../common/toast';
+import { coreRefs } from '../../framework/core_refs';
 
 // import { ObjectFetcher } from '../common/objectFetcher';
 
@@ -73,13 +72,13 @@ export const Home = ({
   coreSavedObjects,
   setBreadcrumbs,
 }: PanelHomeProps) => {
-  const [toasts, setToasts] = useState<Toast[]>([]);
   const [loading, setLoading] = useState(false);
-  const [toastRightSide, setToastRightSide] = useState<boolean>(true);
   const [start, setStart] = useState<ShortDate>('');
   const [end, setEnd] = useState<ShortDate>('');
 
   const dispatch = useDispatch();
+
+  const { setToast } = useToast();
 
   const customPanelBreadCrumbs = [
     ...parentBreadcrumbs,
@@ -89,13 +88,36 @@ export const Home = ({
     },
   ];
 
-  const setToast = (title: string, color = 'success', text?: ReactChild, side?: string) => {
-    if (!text) text = '';
-    setToastRightSide(!side ? true : false);
-    setToasts([...toasts, { id: new Date().toISOString(), title, text, color } as Toast]);
+  const addNewTab = async () => {
+    // get a new tabId
+    const tabId = htmlIdGenerator(TAB_ID_TXT_PFX)();
+
+    // create a new tab
+    await batch(() => {
+      dispatch(initQuery({ tabId }));
+      dispatch(initQueryResult({ tabId }));
+      dispatch(initFields({ tabId }));
+      dispatch(addTab({ tabId }));
+      dispatch(initPatterns({ tabId }));
+    });
+
+    return tabId;
   };
 
-  const onEditClick = (savedVisualizationId: string) => {
+  const onEditClick = async (savedVisualizationId: string) => {
+    // open a new tab in explorer for loading this perticular visualization data to edit
+    const newTabId = await addNewTab();
+    batch(() => {
+      dispatch(
+        changeQuery({
+          tabId: newTabId,
+          query: {
+            [TAB_CREATED_TYPE]: REDIRECT_TAB,
+          },
+        })
+      );
+      dispatch(setSelectedQueryTab({ tabId: newTabId }));
+    });
     window.location.assign(`${observabilityLogsID}#/explorer/${savedVisualizationId}`);
   };
 
@@ -145,14 +167,6 @@ export const Home = ({
 
   return (
     <HashRouter>
-      <EuiGlobalToastList
-        toasts={toasts}
-        dismissToast={(removedToast) => {
-          setToasts(toasts.filter((toast) => toast.id !== removedToast.id));
-        }}
-        side={toastRightSide ? 'right' : 'left'}
-        toastLifeTimeMs={6000}
-      />
       <Switch>
         <Route
           exact
@@ -180,7 +194,6 @@ export const Home = ({
                 pplService={pplService}
                 dslService={dslService}
                 parentBreadcrumbs={customPanelBreadCrumbs}
-                setToast={setToast}
                 onEditClick={onEditClick}
                 page="operationalPanels"
                 coreSavedObjects={coreSavedObjects}
@@ -194,7 +207,6 @@ export const Home = ({
                 chrome={chrome}
                 parentBreadcrumbs={customPanelBreadCrumbs}
                 // renameCustomPanel={renameCustomPanel}
-                setToast={setToast}
                 onEditClick={onEditClick}
                 startTime={start}
                 endTime={end}
