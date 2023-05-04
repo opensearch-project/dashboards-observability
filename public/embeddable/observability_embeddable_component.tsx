79436d07597ad7aa6a76f5f89ea59f70e2644275
Joshua Li <joshuali925@gmail.com>
2023-05-04 21:57:31 +0000

    Add support for ppl visualization
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- public/embeddable/observability_embeddable_component.tsx
+++ public/embeddable/observability_embeddable_component.tsx
@@ -22,6 +22,7 @@ const ObservabilityEmbeddableComponentInner: React.FC<ObservabilityEmbeddableCom
   props
 ) => {
   const visualization = props.output.attributes?.savedVisualization;
+  console.log('❗visualization:', JSON.stringify(visualization));
   return visualization ? (
     <SavedObjectVisualization
       savedVisualization={visualization}
