8f445f14305fe7a0a2973abf398afe2eac2e2849
Simeon Widdis <sawiddis@amazon.com>
2023-05-22 14:44:09 -0700

    Add revised typing for integration objects
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- server/adaptors/integrations/types.ts
+++ server/adaptors/integrations/types.ts
@@ -1,38 +1,72 @@
 interface IntegrationTemplate {
-  templateName: string;
+  name: string;
   version: string;
-  description: string;
-  catalog: string;
-  assetUrl: string;
-  displayAssets: any[];
+  author?: string;
+  description?: string;
+  tags?: string[];
+  sourceUrl?: string;
+  catalog?: string;
+  staticAssetMapping?: StaticAssetMapping;
+  staticAssets?: StaticAsset[]; // Blobs for static assets to display, like logos
+  components: IntegrationComponent[]; // Describes expected format of the data, used for validation
+  displayAssets: IntegrationAsset[]; // Asset objects that can be imported
+}
+
+interface StaticAssetMapping {
+  logo?: string;
+  gallery?: string[];
+  darkModeLogo?: string; // Fallback to light mode if absent
+  darkModeGallery?: string[];
+}
+
+interface StaticAsset {
+  path: string;
+  mimeType: string;
+  annotation?: string;
+  data: string; // Base64 encoded
+}
+
+interface IntegrationComponent {
+  name: string;
+  version: string;
+  description?: string;
+  sourceUrl?: string;
+  schemaBody: string;
+  mappingBody: string;
+}
+
+interface IntegrationAsset {
+  assetBody: string;
 }
 
 interface IntegrationTemplateSearchResult {
-  integrations: IntegrationTemplate[];
+  hits: IntegrationTemplate[];
 }
 
 interface IntegrationTemplateQuery {
-  name?: string; // Temporary value to satisfy linter, don't use
+  tags?: string[];
 }
 
 interface IntegrationInstance {
+  id: string;
   templateName: string;
-  type: string;
+  integrationType: string;
   dataset: string;
   namespace: string;
-  id: string;
-  version: string;
-  description: string;
-  template: string;
   creationDate: string;
-  author: string;
-  status: string;
-  dashboardUrl: string;
-  assets: object;
+  status: string; // Dynamically computed status on read
+  assets: AssetReference[]; // References to imported assets
+}
+
+interface AssetReference {
+  assetType: string;
+  assetUrl: string;
+  status: string; // Aggregated to show status in parent object
+  isDefaultAsset: boolean; // Used to know which one to open by default
 }
 
 interface IntegrationInstanceSearchResult {
-  integrations: IntegrationInstance[];
+  hits: IntegrationInstance[];
 }
 
 interface IntegrationInstanceQuery {
