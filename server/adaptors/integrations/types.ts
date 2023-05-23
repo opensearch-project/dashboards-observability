ea74319b991521f2806536fd96274c4012f9c3b9
Simeon Widdis <sawiddis@amazon.com>
2023-05-23 12:03:23 -0700

    Convert interfaces to types
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- server/adaptors/integrations/types.ts
+++ server/adaptors/integrations/types.ts
@@ -6,73 +6,31 @@ interface IntegrationTemplate {
   description?: string;
   tags?: string[];
   sourceUrl?: string;
-  catalog?: string;
-  statics?: IntegrationStatics;
-  components: IntegrationComponent[]; // Describes expected format of the data, used for validation
-  displayAssets: IntegrationAsset[]; // Asset objects that can be imported
-}
-
-class IntegrationStatics {
-  mapping?: StaticAssetMapping;
-  assets?: StaticAsset[];
-
-  getLogo(darkMode?: boolean): StaticAsset | undefined {
-    if (darkMode && this.mapping?.darkModeLogo) {
-      return this.getAsset(this.mapping?.darkModeLogo);
-    }
-    if (this.mapping?.logo) {
-      return this.getAsset(this.mapping?.logo);
-    }
-  }
-
-  getGallery(darkMode?: boolean): StaticAsset[] {
-    if (darkMode && this.mapping?.darkModeGallery) {
-      return this.mapping.darkModeGallery
-        .map((path) => this.getAsset(path))
-        .filter((x) => x) as StaticAsset[];
-    }
-    if (this.mapping?.gallery) {
-      return this.mapping.gallery
-        .map((path) => this.getAsset(path))
-        .filter((x) => x) as StaticAsset[];
-    }
-    return [];
-  }
-
-  getAsset(path: string): StaticAsset | undefined {
-    for (const asset of this.assets?.values() ?? []) {
-      if (asset.path === path) {
-        return asset;
-      }
-    }
-  }
-}
-
-interface StaticAssetMapping {
-  logo?: string;
-  gallery?: string[];
-  darkModeLogo?: string; // Fallback to light mode if absent
-  darkModeGallery?: string[];
-}
-
-interface StaticAsset {
-  path: string;
-  mimeType: string;
-  annotation?: string;
-  data: string; // Base64 encoded
-}
-
-interface IntegrationComponent {
-  name: string;
-  version: string;
-  description?: string;
-  sourceUrl?: string;
-  schemaBody: string;
-  mappingBody: string;
-}
-
-interface IntegrationAsset {
-  assetBody: string;
+  statics?: {
+    mapping?: {
+      logo?: string;
+      gallery?: string[];
+      darkModeLogo?: string; // Fallback to light mode if absent
+      darkModeGallery?: string[];
+    };
+    assets?: Array<{
+      path: string;
+      mimeType: string;
+      annotation?: string;
+      data: string;
+    }>;
+  };
+  components: Array<{
+    name: string;
+    version: string;
+    description?: string;
+    sourceUrl?: string;
+    schemaBody: string;
+    mappingBody: string;
+  }>;
+  displayAssets: Array<{
+    assetBody: string;
+  }>;
 }
 
 interface IntegrationTemplateSearchResult {
@@ -87,19 +45,20 @@ interface IntegrationInstance {
   id: string;
   name: string;
   templateName: string;
-  integrationType: string;
-  dataSource: string;
+  dataSource: {
+    sourceType: string;
+    dataset: string;
+    namespace: string;
+  };
   creationDate: string;
   tags?: string[];
-  status: string; // Dynamically computed status on read
-  assets: AssetReference[]; // References to imported assets
-}
-
-interface AssetReference {
-  assetType: string;
-  assetUrl: string;
-  status: string; // Aggregated to show status in parent object
-  isDefaultAsset: boolean; // Used to know which one to open by default
+  status: string;
+  assets: Array<{
+    assetType: string;
+    assetUrl: string;
+    status: string;
+    isDefaultAsset: boolean;
+  }>;
 }
 
 interface IntegrationInstanceSearchResult {
