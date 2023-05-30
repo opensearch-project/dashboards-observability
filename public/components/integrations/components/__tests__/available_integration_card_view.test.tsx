372cac709707bac96ea7b8eb95b7633efd1335bf
Derek Ho <dxho@amazon.com>
2023-05-30 10:22:55 -0400

    fix up the testing name
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- public/components/integrations/components/__tests__/available_integration_card_view.test.tsx
+++ public/components/integrations/components/__tests__/available_integration_card_view.test.tsx
@@ -5,15 +5,14 @@
 
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import React from 'react';
 import { waitFor } from '@testing-library/react';
 import { AvailableIntegrationsCardView } from '../available_integration_card_view';
 import { data } from './testing_constants';
 
-describe('Integration Header Test', () => {
+describe('Available Integration Card View Test', () => {
   configure({ adapter: new Adapter() });
 
-  it('Renders integration header as expected', async () => {
+  it('Renders nginx integration card view using dummy data', async () => {
     const wrapper = mount(AvailableIntegrationsCardView(data));
 
     await waitFor(() => {
