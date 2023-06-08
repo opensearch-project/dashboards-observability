10fee4740780f1fee4a70288a68d3d9a9b11d05c
Simeon Widdis <sawiddis@amazon.com>
2023-06-08 10:26:41 -0700

    Add copyright notice to repository_fs
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- server/adaptors/integrations/repository_fs.ts
+++ server/adaptors/integrations/repository_fs.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import * as fs from 'fs/promises';
 import * as path from 'path';
 import { templateValidator } from './validators';
