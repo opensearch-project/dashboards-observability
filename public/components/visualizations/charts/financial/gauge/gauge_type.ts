b36b150d2c0600adc138b5906b5a08925a05cd0c
Anirudha (Ani) Jadhav <anirudha@nyu.edu>
2023-06-29 17:40:54 -0700

    Merge pull request #588 from mengweieric/feature/prerelease-code-cleanup
    (cherry picked from commit a85b716f4069e70cd2736e0fab7ec32a89907f8d)

--- public/components/visualizations/charts/financial/gauge/gauge_type.ts
+++ public/components/visualizations/charts/financial/gauge/gauge_type.ts
@@ -5,7 +5,6 @@
 
 import { Gauge } from './gauge';
 import { getPlotlySharedConfigs, getPlotlyCategory } from '../../shared/shared_configs';
-import { LensIconChartLine } from '../../../assets/chart_line';
 import { VizDataPanel } from '../../../../event_analytics/explorer/visualizations/config_panel/config_panes/default_vis_editor';
 import { ConfigEditor } from '../../../../event_analytics/explorer/visualizations/config_panel/config_panes/json_editor';
 import {
@@ -32,7 +31,6 @@ export const createGaugeTypeDefinition = (params: any = {}) => ({
   selection: {
     dataLoss: 'nothing',
   },
-  icon: LensIconChartLine,
   valueseries: 'yaxis',
   editorconfig: {
     panelTabs: [
