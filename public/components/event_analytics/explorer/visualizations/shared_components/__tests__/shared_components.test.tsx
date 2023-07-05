fed75fb72f1ff154d5a5026052e481e85105fabb
Eric Wei <menwe@amazon.com>
2023-07-05 12:51:03 -0700

    Resolve heatmap issue and update snapshots (#601)
    * resolve heatmap issue and update snapshots
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * resolve test failures and update test snapshots
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    ---------
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    Co-authored-by: Anirudha (Ani) Jadhav <anirudha@nyu.edu>

--- public/components/event_analytics/explorer/visualizations/shared_components/__tests__/shared_components.test.tsx
+++ public/components/event_analytics/explorer/visualizations/shared_components/__tests__/shared_components.test.tsx
@@ -7,22 +7,10 @@ import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { waitFor } from '@testing-library/react';
-import { EmptyPlaceholder } from '../empty_placeholder';
-import { LensIconChartBar } from '../../../../../visualizations/assets/chart_bar';
 import { ToolbarButton } from '../toolbar_button';
 
 describe('Shared components', () => {
   configure({ adapter: new Adapter() });
-
-  it('Renders empty placeholder component', async () => {
-    const wrapper = mount(<EmptyPlaceholder icon={LensIconChartBar} />);
-
-    wrapper.update();
-
-    await waitFor(() => {
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
 
   it('Renders tool bar button component', async () => {
     const handleClick = jest.fn();
