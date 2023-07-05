496f5f0f346e030e3212b518a7e59e68cbd1529a
Eric Wei <menwe@amazon.com>
2023-07-05 14:45:30 -0700

    Update tests and snapshots (#608)
    * code cleanup and snapshots updated
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * few more tests
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    ---------
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    (cherry picked from commit 7fa6c245cde0a519bbfc3a8811a3c14e30093606)

--- public/components/visualizations/charts/maps/heatmap_type.ts
+++ public/components/visualizations/charts/maps/heatmap_type.ts
@@ -5,7 +5,6 @@
 
 import { HeatMap } from './heatmap';
 import { getPlotlySharedConfigs, getPlotlyCategory } from '../shared/shared_configs';
-import { LensIconChartPie } from '../../assets/chart_pie';
 import { VizDataPanel } from '../../../event_analytics/explorer/visualizations/config_panel/config_panes/default_vis_editor';
 import { ConfigEditor } from '../../../event_analytics/explorer/visualizations/config_panel/config_panes/json_editor';
 import { ConfigValueOptions } from '../../../event_analytics/explorer/visualizations/config_panel/config_panes/config_controls';
@@ -24,7 +23,6 @@ export const createMapsVisDefinition = () => ({
   selection: {
     dataLoss: 'nothing',
   },
-  icon: LensIconChartPie,
   editorConfig: {
     panelTabs: [
       {
