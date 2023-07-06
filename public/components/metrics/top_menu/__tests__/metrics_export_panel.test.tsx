ad3f4cea11bf13fd38d0989e623be79c52f0aa1c
Peter Fitzgibbons <peter.fitzgibbons@gmail.com>
2023-07-06 10:50:19 -0700

    IWP MetricsExportPanel test
    Signed-off-by: Peter Fitzgibbons <peter.fitzgibbons@gmail.com>

--- public/components/metrics/top_menu/__tests__/metrics_export_panel.test.tsx
+++ public/components/metrics/top_menu/__tests__/metrics_export_panel.test.tsx
@@ -6,7 +6,8 @@
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { waitFor } from '@testing-library/react';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { screen, waitFor } from '@testing-library/react';
 import httpClientMock from '../../../../../test/__mocks__/httpClientMock';
 import { samplePanelOptions, sampleVisualizationById } from '../../../../../test/metrics_contants';
 import { applyMiddleware, createStore } from '@reduxjs/toolkit';
@@ -19,27 +20,31 @@ import { coreRefs } from '../../../../framework/core_refs';
 
 describe('Export Metrics Panel Component', () => {
   configure({ adapter: new Adapter() });
-  const store = createStore(rootReducer, applyMiddleware(thunk));
-  coreRefs.savedObjectsClient.find = jest.fn(() =>
-    Promise.resolve({
-      savedObjects: [],
-      then: () => Promise.resolve(),
-    })
-  );
+  let store;
 
-  let httpFlag = 1;
-  httpClientMock.get = jest.fn(() => {
-    if (httpFlag === 1) {
-      httpFlag += 1;
-      return Promise.resolve((samplePanelOptions as unknown) as HttpResponse);
-    } else {
-      return Promise.resolve((sampleVisualizationById as unknown) as HttpResponse);
-    }
+  beforeEach(() => {
+    let httpFlag = 1;
+    httpClientMock.get = jest.fn(() => {
+      if (httpFlag === 1) {
+        httpFlag += 1;
+        return Promise.resolve((samplePanelOptions as unknown) as HttpResponse);
+      } else {
+        return Promise.resolve((sampleVisualizationById as unknown) as HttpResponse);
+      }
+    });
+
+    coreRefs.http = httpClientMock;
   });
 
-  coreRefs.http = httpClientMock;
-
   it('renders Export Metrics Panel Component', async () => {
+    store = createStore(rootReducer, applyMiddleware(thunk));
+    coreRefs.savedObjectsClient.find = jest.fn(() =>
+      Promise.resolve({
+        savedObjects: [],
+        then: () => Promise.resolve(),
+      })
+    );
+
     const setIsSavePanelOpen = jest.fn();
 
     const wrapper = mount(
@@ -57,6 +62,57 @@ describe('Export Metrics Panel Component', () => {
     await waitFor(() => {
       expect(wrapper).toMatchSnapshot();
     });
+
+    wrapper.unmount();
+  });
+
+  it('allows change of metric name', async () => {
+    const initialState = {
+      customPanel: {
+        panelList: [],
+      },
+      metrics: {
+        selected: ['test_prom_metric'],
+        metricsLayout: [
+          {
+            id: 'test_prom_metric',
+            savedVisualizationId: 'test_prom_metric',
+            x: 0,
+            y: 0,
+            h: 2,
+            w: 12,
+            metricType: 'prometheusMetric',
+          },
+        ],
+      },
+    };
+
+    const mockStore = configureMockStore();
+    store = mockStore({ ...initialState });
+    store.dispatch = jest.fn();
+
+    const setIsSavePanelOpen = jest.fn();
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <MetricsExportPanel
+          startTime="0"
+          endTime="0"
+          spanValue={1}
+          resolutionValue={'h'}
+          setIsSavePanelOpen={setIsSavePanelOpen}
+        />
+      </Provider>
+    );
+
+    wrapper.update();
+    console.log(wrapper.debug());
+    const nameInput = screen.findByRole('textbox', { name: 'save-panel-name-0' });
+    console.log(nameInput);
+    await waitFor(() => {
+      expect(wrapper.exists('[data-test-subj="metrics__querySaveName"]')).toEqual(true);
+    });
+    expect(wrapper).toMatchSnapshot();
 
     wrapper.unmount();
   });
