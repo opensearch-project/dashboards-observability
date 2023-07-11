a8311f2025ae1134f0624d19d3cf4acf9576bfef
Simeon Widdis <sawiddis@amazon.com>
2023-07-11 15:02:06 -0700

    Remove unused AWS ELB integration
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- common/constants/integrations.ts
+++ common/constants/integrations.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const OPENSEARCH_DOCUMENTATION_URL = 'https://opensearch.org/docs/latest';
+export const OPENSEARCH_DOCUMENTATION_URL = 'https://opensearch.org/docs/latest/integrations/index';
 export const ASSET_FILTER_OPTIONS = ['index-pattern', 'search', 'visualization', 'dashboard'];
 export const INTEGRATION_TEMPLATE_OPTIONS = ['nginx', 'aws_elb'];
 export const INTEGRATION_CATEOGRY_OPTIONS = [
