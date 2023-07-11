aaa2b554c0a80cce84107564d20f9b45d0a8d6b8
Derek Ho <dxho@amazon.com>
2023-07-11 18:52:29 -0400

    fix relative path
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- public/components/integrations/components/available_integration_overview_page.tsx
+++ public/components/integrations/components/available_integration_overview_page.tsx
@@ -16,7 +16,7 @@ import {
 } from '@elastic/eui';
 import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
-import { INTEGRATION_CATEOGRY_OPTIONS } from 'common/constants/integrations';
+import { INTEGRATION_CATEOGRY_OPTIONS } from '../../../../common/constants/integrations';
 import { IntegrationHeader } from './integration_header';
 import { AvailableIntegrationsTable } from './available_integration_table';
 import { AvailableIntegrationsCardView } from './available_integration_card_view';
