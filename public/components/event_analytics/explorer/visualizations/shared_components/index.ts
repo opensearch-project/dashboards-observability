2a71dff3c0ddfb9e3bd41a6eb15ea70cb43b9a82
Eric Wei <menwe@amazon.com>
2023-07-13 21:48:12 +0000

    update snapshots
    Signed-off-by: Eric Wei <menwe@amazon.com>

--- public/components/event_analytics/explorer/visualizations/shared_components/index.ts
+++ public/components/event_analytics/explorer/visualizations/shared_components/index.ts
@@ -4,9 +4,4 @@
  */
 
 export * from './empty_placeholder';
-<<<<<<< HEAD
-export { ToolbarButtonProps, ToolbarButton } from './toolbar_button';
-export { VisCanvassPlaceholder } from './vis_canvass_placeholder'
-=======
-export { VisWorkspaceDefault } from './vis_canvass_placeholder';
->>>>>>> 06ea75fe (Remove unused files (#662))
+export { VisCanvassPlaceholder } from './vis_canvass_placeholder';
