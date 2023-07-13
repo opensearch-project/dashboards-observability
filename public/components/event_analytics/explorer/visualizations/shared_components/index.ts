88aa674f54c61e0f813b5e5adbcf683773240df0
Eric Wei <menwe@amazon.com>
2023-07-13 14:13:24 -0700

    Remove unused files (#662)
    * remove unused files
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    * missing snapshots
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    
    ---------
    
    Signed-off-by: Eric Wei <menwe@amazon.com>
    (cherry picked from commit 06ea75fe311a80bc16242da3ce03f64bd33bb54f)

--- public/components/event_analytics/explorer/visualizations/shared_components/index.ts
+++ public/components/event_analytics/explorer/visualizations/shared_components/index.ts
@@ -4,5 +4,9 @@
  */
 
 export * from './empty_placeholder';
+<<<<<<< HEAD
 export { ToolbarButtonProps, ToolbarButton } from './toolbar_button';
 export { VisCanvassPlaceholder } from './vis_canvass_placeholder'
+=======
+export { VisWorkspaceDefault } from './vis_canvass_placeholder';
+>>>>>>> 06ea75fe (Remove unused files (#662))
