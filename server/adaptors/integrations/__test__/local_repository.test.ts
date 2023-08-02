1ef0d1f2217515bbdb0f53ea5d1148f201037124
Simeon Widdis <sawiddis@amazon.com>
2023-08-02 16:27:06 -0700

    Re-apply formatting to local_repository.test.ts
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- server/adaptors/integrations/__test__/local_repository.test.ts
+++ server/adaptors/integrations/__test__/local_repository.test.ts
@@ -8,27 +8,27 @@ import { Integration } from '../repository/integration';
 import path from 'path';
 import * as fs from 'fs/promises';
 
-describe("The local repository", () => {
-    it("Should only contain valid integration directories or files.", async () => {
-        let directory = path.join(__dirname, '../__data__/repository');
-        const folders = await fs.readdir(directory);
-        await Promise.all(
-            folders.map(async (folder) => {
-                const integPath = path.join(directory, folder);
-                if (!(await fs.lstat(integPath)).isDirectory()) {
-                    // If it's not a directory (e.g. a README), skip it
-                    return Promise.resolve(null);
-                }
-                // Otherwise, all directories must be integrations
-                const integ = new Integration(integPath);
-                await expect(integ.check()).resolves.toBe(true);
-            })
-        );
-    });
+describe('The local repository', () => {
+  it('Should only contain valid integration directories or files.', async () => {
+    const directory = path.join(__dirname, '../__data__/repository');
+    const folders = await fs.readdir(directory);
+    await Promise.all(
+      folders.map(async (folder) => {
+        const integPath = path.join(directory, folder);
+        if (!(await fs.lstat(integPath)).isDirectory()) {
+          // If it's not a directory (e.g. a README), skip it
+          return Promise.resolve(null);
+        }
+        // Otherwise, all directories must be integrations
+        const integ = new Integration(integPath);
+        await expect(integ.check()).resolves.toBe(true);
+      })
+    );
+  });
 
-    it("Should pass deep validation for all local integrations.", async () => {
-        let repository: Repository = new Repository(path.join(__dirname, '../__data__/repository'));
-        let integrations: Integration[] = await repository.getIntegrationList();
-        await Promise.all(integrations.map(i => expect(i.deepCheck()).resolves.toBeTruthy()));
-    });
+  it('Should pass deep validation for all local integrations.', async () => {
+    const repository: Repository = new Repository(path.join(__dirname, '../__data__/repository'));
+    const integrations: Integration[] = await repository.getIntegrationList();
+    await Promise.all(integrations.map((i) => expect(i.deepCheck()).resolves.toBeTruthy()));
+  });
 });
