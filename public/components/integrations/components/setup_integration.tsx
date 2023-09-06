ab30ed0f002b94e773dde7ddffcdebcf3db1208c
Simeon Widdis <sawiddis@amazon.com>
2023-09-06 15:03:25 -0700

    Swap out flyout for hello-world setup page
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

---
+++ public/components/integrations/components/setup_integration.tsx
@@ -0,0 +1,11 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { EuiPageHeader } from '@elastic/eui';
+import React from 'react';
+
+export function SetupIntegrationStepsView() {
+  return <EuiPageHeader>Hello, World!</EuiPageHeader>;
+}
