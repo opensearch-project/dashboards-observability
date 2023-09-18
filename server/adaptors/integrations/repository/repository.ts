cf1ddf88b72f466ec8a5a99a0e5a2bda03aeacb1
Simeon Widdis <sawiddis@gmail.com>
2023-09-18 09:10:59 -0700

    Refactor Integrations Backend to abstract IO (#947)

--- server/adaptors/integrations/repository/repository.ts
+++ server/adaptors/integrations/repository/repository.ts
@@ -3,39 +3,43 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import * as fs from 'fs/promises';
 import * as path from 'path';
-import { Integration } from './integration';
+import { IntegrationReader } from './integration';
+import { FileSystemCatalogDataAdaptor } from './fs_data_adaptor';
 
-export class Repository {
+export class RepositoryReader {
+  reader: CatalogDataAdaptor;
   directory: string;
 
-  constructor(directory: string) {
+  constructor(directory: string, reader?: CatalogDataAdaptor) {
     this.directory = directory;
+    this.reader = reader ?? new FileSystemCatalogDataAdaptor(directory);
   }
 
-  async getIntegrationList(): Promise<Integration[]> {
-    try {
-      const folders = await fs.readdir(this.directory);
-      const integrations = Promise.all(
-        folders.map(async (folder) => {
-          const integPath = path.join(this.directory, folder);
-          if (!(await fs.lstat(integPath)).isDirectory()) {
-            return null;
-          }
-          const integ = new Integration(integPath);
-          return (await integ.check()) ? integ : null;
-        })
-      );
-      return (await integrations).filter((x) => x !== null) as Integration[];
-    } catch (error) {
-      console.error(`Error reading integration directories in: ${this.directory}`, error);
+  async getIntegrationList(): Promise<IntegrationReader[]> {
+    // TODO in the future, we want to support traversing nested directory structures.
+    const folders = await this.reader.findIntegrations();
+    if (!folders.ok) {
+      console.error(`Error reading integration directories in: ${this.directory}`, folders.error);
       return [];
     }
+    const integrations = await Promise.all(
+      folders.value.map((i) => this.getIntegration(path.basename(i)))
+    );
+    return integrations.filter((x) => x !== null) as IntegrationReader[];
   }
 
-  async getIntegration(name: string): Promise<Integration | null> {
-    const integ = new Integration(path.join(this.directory, name));
-    return (await integ.check()) ? integ : null;
+  async getIntegration(name: string): Promise<IntegrationReader | null> {
+    if ((await this.reader.getDirectoryType(name)) !== 'integration') {
+      console.error(`Requested integration '${name}' does not exist`);
+      return null;
+    }
+    const integ = new IntegrationReader(name, this.reader.join(name));
+    const checkResult = await integ.getConfig();
+    if (!checkResult.ok) {
+      console.error(`Integration '${name}' is invalid:`, checkResult.error);
+      return null;
+    }
+    return integ;
   }
 }
