6b824d418b629e724ddbb0a430a360a7f26a8f8d
Eric <menwe@amazon.com>
2023-10-03 20:36:56 +0000

    adopt empty prompt
    Signed-off-by: Eric <menwe@amazon.com>

--- public/components/event_analytics/explorer/direct_query_running.tsx
+++ public/components/event_analytics/explorer/direct_query_running.tsx
@@ -4,20 +4,21 @@
  */
 
 import React from 'react';
-import { EuiButton, EuiCallOut, EuiLink, EuiProgress, EuiSpacer } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiCallOut,
+  EuiLink,
+  EuiProgress,
+  EuiSpacer,
+  EuiEmptyPrompt,
+  EuiLoadingLogo,
+} from '@elastic/eui';
 
 export const DirectQueryRunning = () => {
   return (
-    <EuiCallOut title="Query Processing..." color="warning" iconType="help">
-      <EuiProgress size="xs" color="accent" />
-      <EuiSpacer size="s" />
-      <p>
-        Leaving the page will cancel the query. Query performance can be improved.
-        <EuiLink href="https://opensearch.org/docs/latest/">Learn more</EuiLink>.
-      </p>
-      <EuiButton href="#" color="warning">
-        Cancel
-      </EuiButton>
-    </EuiCallOut>
+    <EuiEmptyPrompt
+      icon={<EuiProgress size="xs" color="accent" />}
+      title={<h2>Query Processing</h2>}
+    />
   );
 };
