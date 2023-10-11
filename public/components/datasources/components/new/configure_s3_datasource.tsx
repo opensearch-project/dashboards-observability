3582aeeea6bc980a7d2c719595f6e2fde200771b
Derek Ho <dxho@amazon.com>
2023-10-11 14:17:55 -0400

    Add callout and modify content for S3 datasource
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- public/components/datasources/components/new/configure_s3_datasource.tsx
+++ public/components/datasources/components/new/configure_s3_datasource.tsx
@@ -13,6 +13,7 @@ import {
   EuiFieldText,
   EuiTextArea,
   EuiSelect,
+  EuiCallOut,
 } from '@elastic/eui';
 import React, { useState } from 'react';
 import {
@@ -87,11 +88,20 @@ export const ConfigureS3Datasource = (props: ConfigureS3DatasourceProps) => {
         </EuiTitle>
         <EuiSpacer size="s" />
         <EuiText size="s" color="subdued">
-          {`Connect to S3 with OpenSearch and OpenSearch Dashboards. `}
+          {`Connect to Amazon S3 via AWS Glue with Amazon EMR as an execution engine. `}
           <EuiLink external={true} href={OPENSEARCH_DOCUMENTATION_URL} target="blank">
             Learn more
           </EuiLink>
         </EuiText>
+        <EuiSpacer size="s" />
+        <EuiCallOut title="Setup Amazon EMR as execution engine first" iconType="iInCircle">
+          <EuiText size="s" color="subdued">
+            {`Connect to Amazon S3 via AWS Glue with Amazon EMR as an execution engine. `}
+            <EuiLink external={true} href={OPENSEARCH_DOCUMENTATION_URL} target="blank">
+              Learn more
+            </EuiLink>
+          </EuiText>
+        </EuiCallOut>
         <EuiSpacer />
         <EuiText>
           <h3>Data source details</h3>
