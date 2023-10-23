a738e0705f908acdab6d605533c8fc9066dd0eac
Derek Ho <dxho@amazon.com>
2023-10-23 14:19:50 -0400

    False as default
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- public/components/datasources/components/manage/access_control_tab.tsx
+++ public/components/datasources/components/manage/access_control_tab.tsx
@@ -110,7 +110,6 @@ export const AccessControlTab = (props: AccessControlTabProps) => {
           <EuiButton
             data-test-subj="createButton"
             onClick={() => setMode(mode === 'view' ? 'edit' : 'view')}
-            fill={false}
           >
             {mode === 'view' ? 'Edit' : 'Cancel'}
           </EuiButton>
