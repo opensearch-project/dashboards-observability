66794c536740cabfe3e33448e087619a129f90e4
Eric <menwe@amazon.com>
2023-10-25 01:22:52 +0000

    supports session id for direct query
    Signed-off-by: Eric <menwe@amazon.com>

---
+++ common/utils/query_session_utils.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ASYNC_QUERY_SESSION_ID } from '../constants/shared';
+
+export const setAsyncSessionId = (value: string | null) => {
+  console.log('value: ', value);
+  if (value !== null) {
+    sessionStorage.setItem(ASYNC_QUERY_SESSION_ID, value);
+  }
+};
+
+export const getAsyncSessionId = () => {
+  return sessionStorage.getItem(ASYNC_QUERY_SESSION_ID);
+};
