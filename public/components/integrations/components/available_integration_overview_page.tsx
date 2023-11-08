ebcd7c0bb54b0c29cbe06c3653d14e8142c8b96a
Eric <menwe@amazon.com>
2023-11-08 21:31:21 +0000

    Revert "Fix integration labeling to identify S3 integrations (#1157) (#1164)"
    This reverts commit 9de9aea11496408178f6a6c0a4ea8e5320262cdb.
    
    Signed-off-by: Eric <menwe@amazon.com>

--- public/components/integrations/components/available_integration_overview_page.tsx
+++ public/components/integrations/components/available_integration_overview_page.tsx
@@ -117,9 +117,7 @@ export function AvailableIntegrationOverviewPage(props: AvailableIntegrationOver
     http.get(`${INTEGRATIONS_BASE}/repository`).then((exists) => {
       setData(exists.data);
 
-      let newItems = exists.data.hits.flatMap(
-        (hit: { labels?: string[] }) => hit.labels?.sort() ?? []
-      );
+      let newItems = exists.data.hits.flatMap((hit: { labels?: string[] }) => hit.labels ?? []);
       newItems = [...new Set(newItems)].sort().map((newItem) => {
         return {
           name: newItem,
@@ -184,7 +182,13 @@ export function AvailableIntegrationOverviewPage(props: AvailableIntegrationOver
         {isCardView
           ? AvailableIntegrationsCardView({
               data: {
-                hits: data.hits.filter((hit) => helper.every((tag) => hit.labels?.includes(tag))),
+                hits: data.hits.filter((hit) =>
+                  helper.every((compon) =>
+                    hit.components
+                      .map((x) => x.name.split('_').findLast(() => true))
+                      .includes(compon)
+                  )
+                ),
               },
               isCardView,
               setCardView,
@@ -196,7 +200,13 @@ export function AvailableIntegrationOverviewPage(props: AvailableIntegrationOver
           : AvailableIntegrationsTable({
               loading: false,
               data: {
-                hits: data.hits.filter((hit) => helper.every((tag) => hit.labels?.includes(tag))),
+                hits: data.hits.filter((hit) =>
+                  helper.every((compon) =>
+                    hit.components
+                      .map((x) => x.name.split('_').findLast(() => true))
+                      .includes(compon)
+                  )
+                ),
               },
               isCardView,
               setCardView,
