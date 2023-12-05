0a7ef66f28e1799b9317882e56bd30c240d27898
Melissa Vagi <vagimeli@amazon.com>
2023-12-05 13:25:19 -0700

    Update public/components/common/helpers/ppl_docs/commands/syntax.ts
    Co-authored-by: Nathan Bower <nbower@amazon.com>
    Signed-off-by: Melissa Vagi <vagimeli@amazon.com>

--- public/components/common/helpers/ppl_docs/commands/syntax.ts
+++ public/components/common/helpers/ppl_docs/commands/syntax.ts
@@ -8,7 +8,7 @@ export const syntaxCmd = `## Syntax
 ### Command order
 
 The PPL query starts with a \`search\` command to reference a table to search.
-Commans cna be in any order. For example, in the following query, the \`search\` command referencesthe \`accounts\` index as the source, then uses fields and a \`where\` command to do further processing. 
+Commands can be in any order. For example, in the following query, the \`search\` command references the \`accounts\` index as the source and then uses fields and a \`where\` command to perform further processing. 
 
 \`\`\` 
 search source=accounts
