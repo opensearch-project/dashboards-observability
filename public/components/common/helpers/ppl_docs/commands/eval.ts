f696c5fc2eab7ad53bd4e07544a9ee0844f38634
Melissa Vagi <vagimeli@amazon.com>
2023-12-05 12:50:47 -0700

    Update public/components/common/helpers/ppl_docs/commands/eval.ts
    Co-authored-by: Nathan Bower <nbower@amazon.com>
    Signed-off-by: Melissa Vagi <vagimeli@amazon.com>

--- public/components/common/helpers/ppl_docs/commands/eval.ts
+++ public/components/common/helpers/ppl_docs/commands/eval.ts
@@ -64,5 +64,5 @@ The following PPL example shows how to use \`eval\` to create a new field based 
     +-------+-------------+---------+
 
 ### Limitation
-The \`eval\` command is not rewritten to Query DSL. It's only run on the coordinating node.
+The \`eval\` command is not rewritten to query DSL. It is only run on the coordinating node.
 `;
