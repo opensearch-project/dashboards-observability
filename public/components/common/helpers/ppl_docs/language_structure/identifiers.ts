950e4507484dee7a6ad9077f7f00fa8560582181
Melissa Vagi <vagimeli@amazon.com>
2023-12-06 15:14:39 -0700

    Address editorial feedback Identifiers section
    Signed-off-by: Melissa Vagi <vagimeli@amazon.com>

--- public/components/common/helpers/ppl_docs/language_structure/identifiers.ts
+++ public/components/common/helpers/ppl_docs/language_structure/identifiers.ts
@@ -8,31 +8,22 @@ export const pplIdentifiers = `## Indentifiers
 
 ### Identifiers
 
-Identifiers are used for naming database objects, such as an index
-name, a field name, or a custom label. The two types of
-identifiers are _regular identifiers_ and _delimited identifiers_.
-identifiers are _regular identifiers_ and _delimited identifiers_.
+Identifiers are used for naming database objects, such as an index name, a field name, or a custom label. The two types of identifiers are _regular identifiers_ and _delimited identifiers_.
 
 #### Regular identifiers
 
-A regular identifier is a string of characters that starts with an
-ASCII letter (lowercase or uppercase). The subsequent characters can be a
-combination of letters, digits, or underscores (\`_\`). A regular identifier cannot be a reversed keyword, and white space or other special characters are not allowed.
-combination of letters, digits, or underscores (\`_\`). A regular identifier cannot be a reversed keyword, and white space or other special characters are not allowed.
+A regular identifier is a string of characters that starts with an ASCII letter (lowercase or uppercase). The subsequent characters can be a combination of letters, digits, or underscores (\`_\`). A regular identifier cannot be a reversed keyword, and white space or other special characters are not allowed.
 
 The following identifiers are supported by OpenSearch extensions:
 
 - **Identifiers prefixed by dot \`.\`:** This is called a hidden index. An example is \`.opensearch_dashboards\`.
 - **Identifiers prefixed by the \`@\` symbol:** This is common in meta fields generated for data ingestion.
 - **Identifiers with \`-\` in the middle:** This is common in index naming conventions with date information.
-- **Identifiers with the asterisk \`*\` symbol:** This is common for wildcard matches in an index pattern.
+- **Identifiers with the \`*\` symbol:** This is common in wildcard matches in an index pattern.
 
-Index names with a date suffix separated by dashes or dots, such as
-\`cwl-2020.01.11\` or \`logs-7.0-2020.01.11\`, are common in data ingestion. Identifiers used as index names do not need to be enclosed in quotation marks. Additionally, wildcards within date patterns are accepted, enabling data retrieval across indexes covering different date ranges. For example, you can use \`logs-2020.1*\` to search in indexes for October, November,
-and December 2020.
+Index names with a date suffix separated by dashes or dots, such as \`cwl-2020.01.11\` or \`logs-7.0-2020.01.11\`, are common in data ingestion. Identifiers used as index names do not need to be enclosed in quotation marks. Additionally, wildcards within date patterns are accepted, enabling data retrieval across indexes covering different date ranges. For example, you can use \`logs-2020.1*\` to search in indexes for October, November, and December 2020.
 
-#### Example PPL query
-#### Example PPL query
+#### Example 1: Index pattern without quotes 
 
 The following example PPL query uses an index pattern directly without quotes:
 
@@ -55,12 +46,11 @@ A delimited identifier is an identifier enclosed in backticks \`\` that contains
 
 Common use cases for delimited identifiers include the following:
 
-- Identifiers that coincide with reserved keywords
+- Identifiers that coincide with reserved keywords.
 - Identifiers that contain a dot \`.\` or a dash \`-\` need to be distinguished from regular identifiers with qualifiers. Enclosing such identifiers in backticks \`\` allows for the parser to differentiate them from qualified identifiers and enables date information within index names.
 - Identifiers with special characters in index names. Note that OpenSearch permits the use of special characters, including Unicode characters.
 
-#### Example PPL query
-#### Example PPL query
+#### Example 2: Index name enclosed in backticks
 
 The following example PPL query uses an index name enclosed in backticks \`\`:
 
