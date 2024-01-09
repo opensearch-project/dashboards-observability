418fb5a052baaad9cc6c8c12d4588a0172eace9a
Shenoy Pratik <sgguruda@amazon.com>
2024-01-09 10:39:52 -0800

    update para_query_grid
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- public/components/notebooks/components/paragraph_components/para_query_grid.tsx
+++ public/components/notebooks/components/paragraph_components/para_query_grid.tsx
@@ -7,27 +7,19 @@ import { EuiDataGrid, EuiLoadingSpinner, EuiSpacer } from '@elastic/eui';
 import $ from 'jquery';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-type QueryDataGridProps = {
-  rowCount: number,
-  queryColumns: Array<any>,
-  visibleColumns: Array<any>,
-  setVisibleColumns: (visibleColumns: string[]) => void,
-  dataValues: Array<any>,
+interface QueryDataGridProps {
+  rowCount: number;
+  queryColumns: any[];
+  dataValues: any[];
 }
 
-type RenderCellValueProps = {
-  rowIndex: number,
-  columnId: string
+interface RenderCellValueProps {
+  rowIndex: number;
+  columnId: string;
 }
 
 function QueryDataGrid(props: QueryDataGridProps) {
-  const {
-    rowCount,
-    queryColumns,
-    visibleColumns,
-    setVisibleColumns,
-    dataValues,
-  } = props;
+  const { rowCount, queryColumns, dataValues } = props;
   const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 10 });
   // ** Sorting config
   const [sortingColumns, setSortingColumns] = useState([]);
@@ -36,16 +28,16 @@ function QueryDataGrid(props: QueryDataGridProps) {
   const [isVisible, setIsVisible] = useState(false);
 
   const onSort = useCallback(
-    (sortingColumns) => {
-      setSortingColumns(sortingColumns);
+    (newColumns) => {
+      setSortingColumns(newColumns);
     },
     [setSortingColumns]
   );
 
   const onChangeItemsPerPage = useCallback(
     (pageSize) =>
-      setPagination((pagination) => ({
-        ...pagination,
+      setPagination((newPagination) => ({
+        ...newPagination,
         pageSize,
         pageIndex: 0,
       })),
@@ -53,28 +45,24 @@ function QueryDataGrid(props: QueryDataGridProps) {
   );
 
   const onChangePage = useCallback(
-    (pageIndex) =>
-      setPagination((pagination) => ({ ...pagination, pageIndex })),
+    (pageIndex) => setPagination((newPagination) => ({ ...newPagination, pageIndex })),
     [setPagination]
   );
 
   const renderCellValue = useMemo(() => {
     return ({ rowIndex, columnId }: RenderCellValueProps) => {
-      return dataValues.hasOwnProperty(rowIndex)
-        ? dataValues[rowIndex][columnId]
-        : null;
+      return dataValues.hasOwnProperty(rowIndex) ? dataValues[rowIndex][columnId] : null;
     };
   }, []);
 
-  const getUpdatedVisibleColumns = (queryColumns: Array<Object>) => {
-    let updatedVisibleColumns = [];
+  const getUpdatedVisibleColumns = () => {
+    const updatedVisibleColumns = [];
     for (let index = 0; index < queryColumns.length; ++index) {
       updatedVisibleColumns.push(queryColumns[index].displayAsText);
     }
     return updatedVisibleColumns;
-  }
+  };
 
-  
   useEffect(() => {
     if ($('.euiDataGrid__overflow').is(':visible')) {
       setIsVisible(true);
@@ -84,12 +72,12 @@ function QueryDataGrid(props: QueryDataGridProps) {
         setIsVisible(true);
       }
     }, 1000);
-    setVisibleColumns(getUpdatedVisibleColumns(queryColumns));
+    setVisibleColumns(getUpdatedVisibleColumns());
   }, []);
 
-  const displayLoadingSpinner = (!isVisible) ? (
+  const displayLoadingSpinner = !isVisible ? (
     <>
-      <EuiLoadingSpinner size="xl"/>
+      <EuiLoadingSpinner size="xl" />
       <EuiSpacer />
     </>
   ) : null;
@@ -98,7 +86,7 @@ function QueryDataGrid(props: QueryDataGridProps) {
     <div id="queryDataGrid">
       {displayLoadingSpinner}
       <EuiDataGrid
-        aria-label='Query datagrid'
+        aria-label="Query datagrid"
         columns={queryColumns}
         columnVisibility={{ visibleColumns, setVisibleColumns }}
         rowCount={rowCount}
@@ -108,12 +96,12 @@ function QueryDataGrid(props: QueryDataGridProps) {
         pagination={{
           ...pagination,
           pageSizeOptions: [10, 20, 50],
-          onChangeItemsPerPage: onChangeItemsPerPage,
-          onChangePage: onChangePage,
+          onChangeItemsPerPage,
+          onChangePage,
         }}
       />
     </div>
-  )
+  );
 }
 
 function queryDataGridPropsAreEqual(prevProps: QueryDataGridProps, nextProps: QueryDataGridProps) {
@@ -124,4 +112,4 @@ function queryDataGridPropsAreEqual(prevProps: QueryDataGridProps, nextProps: Qu
   );
 }
 
-export const QueryDataGridMemo = React.memo(QueryDataGrid, queryDataGridPropsAreEqual)
+export const QueryDataGridMemo = React.memo(QueryDataGrid, queryDataGridPropsAreEqual);
