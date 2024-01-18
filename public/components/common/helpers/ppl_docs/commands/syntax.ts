57db4dcae5b7a6a14f6c3834c85a20fda76be3e0
Joshua Li <joshuali925@gmail.com>
2024-01-18 11:14:26 -0800

    fix syntax error from #1233 (#1372)
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- public/components/common/helpers/ppl_docs/commands/syntax.ts
+++ public/components/common/helpers/ppl_docs/commands/syntax.ts
@@ -8,9 +8,9 @@ export const syntaxCmd = `## Syntax
 ### Command order
 
 The PPL query starts with a \`search\` command to reference a table to search.
-Commands can be in any order. For example, in the following query, the \`search\` command references the \`accounts\` index as the source and then uses fields and a \`where\` command to perform further processing. 
+Commands can be in any order. For example, in the following query, the \`search\` command references the \`accounts\` index as the source and then uses fields and a \`where\` command to perform further processing.
 
-\`\`\` 
+\`\`\`
 search source=accounts
 | where age > 18
 | fields firstname, lastname
@@ -18,5 +18,4 @@ search source=accounts
 
 ### Required and optional arguments
 
-Required arguments are enclosed in angle brackets \< \>, and optional arguments are enclosed in square brackets \[ \]`.
-;
+Required arguments are enclosed in angle brackets \< \>, and optional arguments are enclosed in square brackets \[ \].`;
