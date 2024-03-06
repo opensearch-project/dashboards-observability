1b005d450779d05030c5cac97c3435ce54d0eb28
Jialiang Liang <jiallian@amazon.com>
2024-03-06 13:24:23 -0800

    [Feature] Associated objects searchbar filters (#1474)
    * Add associated objects filters for databases and accelerations
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Fix both filters
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Fix some UI details
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Finalize the UI details
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Fix the acceleration filter type
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Switch to lodash
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Enable multiple filter selections 1
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Make the filters logic into OR
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Fix some comments
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Switch back to JS instead of using lodash
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Update the snapshots
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Remove the inline style
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Set up an util file
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Add a todo comment for dummy data
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Update the snapshots
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Address some minor comments
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Assign types for obj and clauses
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Setup the consts and replace them
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Finish an i18n example
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Finish the i18n for AO tab
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * i18n done
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    * Add jest test
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>
    
    ---------
    
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- public/components/datasources/components/manage/associated_objects/associated_objects_tab.tsx
+++ public/components/datasources/components/manage/associated_objects/associated_objects_tab.tsx
@@ -4,6 +4,7 @@
  */
 
 import React, { useEffect, useState } from 'react';
+import _ from 'lodash';
 import {
   EuiInMemoryTable,
   EuiLink,
@@ -15,18 +16,53 @@ import {
   EuiButton,
   EuiSpacer,
   EuiEmptyPrompt,
+  SearchFilterConfig,
+  EuiTableFieldDataColumnType,
 } from '@elastic/eui';
 import { AssociatedObject } from 'common/types/data_connections';
+import { i18n } from '@osd/i18n';
 import { AccelerationsRecommendationCallout } from './accelerations_recommendation_callout';
+import {
+  ASSC_OBJ_TABLE_ACC_COLUMN_NAME,
+  ASSC_OBJ_TABLE_SEARCH_HINT,
+  ASSC_OBJ_PANEL_TITLE,
+  ASSC_OBJ_PANEL_DESRIPTION,
+  ASSC_OBJ_NO_DATA_TITLE,
+  ASSC_OBJ_NO_DATA_DESCRIPTION,
+  ASSC_OBJ_REFRESH_BTN,
+  ASSC_OBJ_FRESH_MSG,
+  ASSC_OBJ_TABLE_SUBJ,
+} from './utils/associated_objects_tab_utils';
 
 interface AssociatedObjectsTabProps {
   associatedObjects: AssociatedObject[];
+}
+
+interface FilterOption {
+  value: string;
+  text: string;
+}
+
+interface AssociatedTableFilter {
+  type: string;
+  field: string;
+  operator: string;
+  value: string;
+}
+
+function isClauseMatched(record: AssociatedObject, filterObj: AssociatedTableFilter): boolean {
+  const entries = Object.entries(record);
+
+  return entries.some(([key, value]) => key === filterObj.field && filterObj.value === value);
 }
 
 export const AssociatedObjectsTab: React.FC<AssociatedObjectsTabProps> = ({
   associatedObjects,
 }) => {
   const [lastUpdated, setLastUpdated] = useState('');
+  const [databaseFilterOptions, setDatabaseFilterOptions] = useState<FilterOption[]>([]);
+  const [accelerationFilterOptions, setAccelerationFilterOptions] = useState<FilterOption[]>([]);
+  const [filteredObjects, setFilteredObjects] = useState<AssociatedObject[]>([]);
 
   // TODO: FINISH THE REFRESH LOGIC
   const fetchAssociatedObjects = async () => {
@@ -39,35 +75,59 @@ export const AssociatedObjectsTab: React.FC<AssociatedObjectsTabProps> = ({
 
   useEffect(() => {
     fetchAssociatedObjects();
-  }, []);
+
+    const databaseOptions = Array.from(new Set(associatedObjects.map((obj) => obj.database)))
+      .sort()
+      .map((database) => ({ value: database, text: database }));
+    setDatabaseFilterOptions(databaseOptions);
+
+    const accelerationOptions = Array.from(
+      new Set(associatedObjects.flatMap((obj) => obj.accelerations).filter(Boolean))
+    )
+      .sort()
+      .map((acceleration) => ({ value: acceleration, text: acceleration }));
+    setAccelerationFilterOptions(accelerationOptions);
+
+    setFilteredObjects(associatedObjects);
+  }, [associatedObjects]);
 
   const AssociatedObjectsHeader = () => {
+    const panelTitle = i18n.translate('datasources.associatedObjectsTab.panelTitle', {
+      defaultMessage: ASSC_OBJ_PANEL_TITLE,
+    });
+
+    const panelDescription = i18n.translate('datasources.associatedObjectsTab.panelDescription', {
+      defaultMessage: ASSC_OBJ_PANEL_DESRIPTION,
+    });
+
     return (
       <EuiFlexGroup direction="row">
         <EuiFlexItem>
           <EuiText size="m">
-            <h2 className="panel-title">Associated objects</h2>
-            Manage objects associated with this data sources.
+            <h2 className="panel-title">{panelTitle}</h2>
+            {panelDescription}
           </EuiText>
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
-          <div style={{ textAlign: 'right' }}>
-            <EuiText color="subdued" style={{ fontSize: 'small' }}>
-              Last updated at:
-            </EuiText>
-            <EuiText color="subdued" style={{ fontSize: 'small' }}>
-              {lastUpdated}
-            </EuiText>
-          </div>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiButton
-            data-test-subj="freshButton"
-            iconType="refresh"
-            onClick={fetchAssociatedObjects}
-          >
-            Refresh
-          </EuiButton>
+          <EuiFlexGroup direction="rowReverse" alignItems="flexEnd">
+            <EuiFlexItem grow={false}>
+              <EuiButton
+                data-test-subj="freshButton"
+                iconType="refresh"
+                onClick={fetchAssociatedObjects}
+              >
+                {ASSC_OBJ_REFRESH_BTN}
+              </EuiButton>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiText textAlign="right" size="xs" color="subdued">
+                {ASSC_OBJ_FRESH_MSG}
+              </EuiText>
+              <EuiText textAlign="right" color="subdued" size="xs">
+                {lastUpdated}
+              </EuiText>
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </EuiFlexItem>
       </EuiFlexGroup>
     );
@@ -75,8 +135,20 @@ export const AssociatedObjectsTab: React.FC<AssociatedObjectsTabProps> = ({
 
   const noDataMessage = (
     <EuiEmptyPrompt
-      title={<h2>You have no associated objects</h2>}
-      body={<p>Add or config tables from your data source or use Query Workbench.</p>}
+      title={
+        <h2>
+          {i18n.translate('datasources.associatedObjectsTab.noDataTitle', {
+            defaultMessage: ASSC_OBJ_NO_DATA_TITLE,
+          })}
+        </h2>
+      }
+      body={
+        <p>
+          {i18n.translate('datasources.associatedObjectsTab.noDataDescription', {
+            defaultMessage: ASSC_OBJ_NO_DATA_DESCRIPTION,
+          })}
+        </p>
+      }
       actions={
         <EuiButton
           color="primary"
@@ -85,7 +157,9 @@ export const AssociatedObjectsTab: React.FC<AssociatedObjectsTabProps> = ({
           iconType="popout"
           iconSide="left"
         >
-          Query Workbench
+          {i18n.translate('datasources.associatedObjectsTab.queryWorkbenchButton', {
+            defaultMessage: 'Query Workbench',
+          })}
         </EuiButton>
       }
     />
@@ -94,68 +168,150 @@ export const AssociatedObjectsTab: React.FC<AssociatedObjectsTabProps> = ({
   const columns = [
     {
       field: 'name',
-      name: 'Name',
+      name: i18n.translate('datasources.associatedObjectsTab.column.name', {
+        defaultMessage: 'Name',
+      }),
       sortable: true,
       'data-test-subj': 'nameCell',
-      render: (name: string) => (
-        <EuiLink href="https://example.com" target="_blank">
-          {name}
-        </EuiLink>
-      ),
+      render: (name: string) => <EuiLink href="https://example.com">{name}</EuiLink>,
     },
     {
       field: 'database',
-      name: 'Database',
-      truncateText: true,
-      render: (database: string) => (
-        <EuiLink href="https://example.com" target="_blank">
-          {database}
-        </EuiLink>
-      ),
+      name: i18n.translate('datasources.associatedObjectsTab.column.database', {
+        defaultMessage: 'Database',
+      }),
+      sortable: true,
     },
     {
       field: 'type',
-      name: 'Type',
+      name: i18n.translate('datasources.associatedObjectsTab.column.type', {
+        defaultMessage: 'Type',
+      }),
       sortable: true,
     },
     {
       field: 'createdByIntegration',
-      name: 'Created by Integration',
+      name: i18n.translate('datasources.associatedObjectsTab.column.createdByIntegration', {
+        defaultMessage: 'Created by Integration',
+      }),
       sortable: true,
+      render: (createdByIntegration: string, _item: AssociatedObject) =>
+        createdByIntegration ? (
+          <EuiLink onClick={() => openDetailsPage(createdByIntegration)}>
+            {createdByIntegration}
+          </EuiLink>
+        ) : (
+          '-'
+        ),
     },
     {
       field: 'accelerations',
-      name: 'Accelerations',
+      name: i18n.translate('datasources.associatedObjectsTab.column.accelerations', {
+        defaultMessage: 'Accelerations',
+      }),
       sortable: true,
+      render: (accelerations: string[]) => {
+        return accelerations.length > 0
+          ? accelerations.map((acceleration, index) => (
+              <React.Fragment key={index}>
+                <EuiLink onClick={() => openFlyout(acceleration)}>{acceleration}</EuiLink>
+                {index < accelerations.length - 1 ? ', ' : ''}
+              </React.Fragment>
+            ))
+          : '-';
+      },
     },
     {
-      name: 'Actions',
+      name: i18n.translate('datasources.associatedObjectsTab.column.actions', {
+        defaultMessage: 'Actions',
+      }),
       actions: [
         {
-          name: 'Edit',
-          description: 'Edit this object',
+          name: i18n.translate('datasources.associatedObjectsTab.action.discover.name', {
+            defaultMessage: 'Discover',
+          }),
+          description: i18n.translate(
+            'datasources.associatedObjectsTab.action.discover.description',
+            {
+              defaultMessage: 'Discover this object',
+            }
+          ),
           type: 'icon',
           icon: 'discoverApp',
-          onClick: (item: AssociatedObject) => console.log('Edit', item),
+          onClick: (item: AssociatedObject) => console.log('Discover', item),
         },
         {
-          name: 'Delete',
-          description: 'Delete this object',
+          name: i18n.translate('datasources.associatedObjectsTab.action.accelerate.name', {
+            defaultMessage: 'Accelerate',
+          }),
+          description: i18n.translate(
+            'datasources.associatedObjectsTab.action.accelerate.description',
+            {
+              defaultMessage: 'Accelerate this object',
+            }
+          ),
           type: 'icon',
           icon: 'bolt',
-          onClick: (item: AssociatedObject) => console.log('Delete', item),
+          available: (item: AssociatedObject) => item.type === 'Table',
+          onClick: (item: AssociatedObject) => console.log('Accelerate', item),
         },
       ],
     },
-  ];
+  ] as Array<EuiTableFieldDataColumnType<any>>;
+
+  const onSearchChange = ({ query, error }) => {
+    if (error) {
+      console.log('Search error:', error);
+      return;
+    }
+
+    const matchesClauses = (obj: AssociatedObject, clauses: AssociatedTableFilter[]): boolean => {
+      if (clauses.length === 0) return true;
+      return clauses.some((clause) => {
+        if (clause.type !== 'field' && clause.field !== ASSC_OBJ_TABLE_ACC_COLUMN_NAME) return true;
+        if (clause.field === ASSC_OBJ_TABLE_ACC_COLUMN_NAME)
+          return obj[ASSC_OBJ_TABLE_ACC_COLUMN_NAME].includes(clause.value);
+        return isClauseMatched(obj, clause);
+      });
+    };
+
+    const filtered = associatedObjects.filter((obj) => {
+      const clauses = query.ast._clauses;
+      return matchesClauses(obj, clauses);
+    });
+
+    setFilteredObjects(filtered);
+  };
+
+  const searchFilters = [
+    {
+      type: 'field_value_selection',
+      field: 'database',
+      name: 'Database',
+      multiSelect: true,
+      options: databaseFilterOptions,
+      cache: 60000,
+    },
+    {
+      type: 'field_value_selection',
+      field: 'accelerations',
+      name: 'Accelerations',
+      multiSelect: true,
+      options: accelerationFilterOptions,
+      cache: 60000,
+    },
+  ] as SearchFilterConfig[];
 
   const search = {
+    filters: searchFilters,
     box: {
       incremental: true,
+      placeholder: ASSC_OBJ_TABLE_SEARCH_HINT,
       schema: {
         fields: { name: { type: 'string' }, database: { type: 'string' } },
       },
     },
+    onChange: onSearchChange,
   };
 
   const pagination = {
@@ -180,12 +336,13 @@ export const AssociatedObjectsTab: React.FC<AssociatedObjectsTabProps> = ({
         <EuiSpacer />
         {associatedObjects.length > 0 ? (
           <EuiInMemoryTable
-            items={associatedObjects}
+            items={filteredObjects}
             columns={columns}
             search={search}
             pagination={pagination}
             sorting={sorting}
             noItemsMessage={associatedObjects.length === 0 ? noDataMessage : undefined}
+            data-test-subj={ASSC_OBJ_TABLE_SUBJ}
           />
         ) : (
           noDataMessage
