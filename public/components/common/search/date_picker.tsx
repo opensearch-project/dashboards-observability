73899a2f20c1df4285bf43ce7745ed1a169a4679
Paul Sebastian <paulstn@amazon.com>
2024-03-19 13:56:06 -0700

    Implement results from no timestamp in Explorer (#1554)
    * history location state retrieval
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * small changes to see results from no timestamp
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * remove time field if there is no default timestamp
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * explorer events ui update
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * changes to disable timepicker
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * modify timepicker logic to disappear instead of disable
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * allow for visualizations without timestamps
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * allow for saved queries to load with no timestamp
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * fix linting complaints
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * change naming convention
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * add new no timestamp jest test
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    * stop app analytics from crashing
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    
    ---------
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>

--- public/components/common/search/date_picker.tsx
+++ public/components/common/search/date_picker.tsx
@@ -5,6 +5,7 @@
 
 import { EuiSuperDatePicker, EuiToolTip } from '@elastic/eui';
 import React from 'react';
+import { i18n } from '@osd/i18n';
 import { uiSettingsService } from '../../../../common/utils';
 import { coreRefs } from '../../../framework/core_refs';
 import { IDatePickerProps } from './search';
@@ -23,28 +24,40 @@ export function DatePicker(props: IDatePickerProps) {
   } = props;
 
   const handleTimeChange = (e: any) => handleTimePickerChange([e.start, e.end]);
-  const allowTimeChanging = !coreRefs.queryAssistEnabled || isAppAnalytics;
+
+  let finalizedStartTime;
+  let finalizedEndTime;
+  let setDisabled;
+  let toolTipMessage;
+
+  if (coreRefs.queryAssistEnabled && !isAppAnalytics) {
+    // is query assistant inside log explorer
+    finalizedStartTime = QUERY_ASSIST_START_TIME;
+    finalizedEndTime = QUERY_ASSIST_END_TIME;
+    setDisabled = true;
+    toolTipMessage = i18n.translate('discover.queryAssistant.timePickerDisabledMessage', {
+      defaultMessage: 'Date range has been disabled to accomodate timerange of all datasets',
+    });
+  } else {
+    finalizedStartTime = startTime;
+    finalizedEndTime = endTime;
+    setDisabled = false;
+    toolTipMessage = false;
+  }
 
   return (
     <>
-      <EuiToolTip
-        position="bottom"
-        content={
-          allowTimeChanging
-            ? false
-            : 'Date range has been disabled to accomodate timerange of all datasets'
-        }
-      >
+      <EuiToolTip position="bottom" content={toolTipMessage}>
         <EuiSuperDatePicker
           data-test-subj="pplSearchDatePicker"
-          start={allowTimeChanging ? startTime : QUERY_ASSIST_START_TIME}
-          end={allowTimeChanging ? endTime : QUERY_ASSIST_END_TIME}
+          start={finalizedStartTime}
+          end={finalizedEndTime}
           dateFormat={uiSettingsService.get('dateFormat')}
           onTimeChange={handleTimeChange}
           onRefresh={handleTimeRangePickerRefresh}
           className="osdQueryBar__datePicker"
           showUpdateButton={false}
-          isDisabled={!allowTimeChanging}
+          isDisabled={setDisabled}
         />
       </EuiToolTip>
     </>
