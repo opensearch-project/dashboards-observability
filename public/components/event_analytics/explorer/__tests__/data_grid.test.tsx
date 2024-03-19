2401f89d08df99036e9ec894d68dfa3d18b271d0
Paul Sebastian <paulstn@amazon.com>
2024-03-19 13:01:15 -0700

    add new no timestamp jest test
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>

--- public/components/event_analytics/explorer/__tests__/data_grid.test.tsx
+++ public/components/event_analytics/explorer/__tests__/data_grid.test.tsx
@@ -132,4 +132,53 @@ describe('Datagrid component', () => {
       expect(wrapper).toMatchSnapshot();
     });
   });
+
+  it('renders data grid with no timestamp', async () => {
+    const explorerFields = {
+      [SELECTED_FIELDS]: [],
+      [UNSELECTED_FIELDS]: [],
+      [AVAILABLE_FIELDS]: SIDEBAR_AVAILABLE_FIELDS,
+      [QUERIED_FIELDS]: QUERY_FIELDS,
+    };
+
+    coreStartMock.http.get = jest
+      .fn()
+      .mockResolvedValue((sampleEmptyPanel as unknown) as HttpResponse);
+
+    const tabId = 'explorer-tab-_fbef9141-48eb-11ee-a60a-af33302cfb3c';
+
+    const pplService = new PPLService(coreStartMock.http);
+    const preloadedState = {
+      queries: {
+        [tabId]: {
+          [SELECTED_TIMESTAMP]: '',
+        },
+      },
+    };
+    const store = configureStore({ reducer: queriesReducer, preloadedState });
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <DataGrid
+          http={coreStartMock.http}
+          pplService={pplService}
+          rows={DATA_GRID_ROWS}
+          explorerFields={explorerFields}
+          timeStampField={''}
+          rawQuery={EXPLORER_DATA_GRID_QUERY}
+          totalHits={1390}
+          requestParams={{ tabId }}
+          startTime={'now/y'}
+          endTime={'now'}
+          storedSelectedColumns={DEFAULT_EMPTY_EXPLORER_FIELDS}
+        />
+      </Provider>
+    );
+
+    wrapper.update();
+
+    await waitFor(() => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
 });
