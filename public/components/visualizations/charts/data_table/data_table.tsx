e4ed9ca73bdd81845435b42a1265d9bf997b3032
Simeon Widdis <sawiddis@amazon.com>
2024-03-21 14:19:54 -0700

    Shuffle imports and update style import paths
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- public/components/visualizations/charts/data_table/data_table.tsx
+++ public/components/visualizations/charts/data_table/data_table.tsx
@@ -8,8 +8,8 @@ import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
 // ag-data-grid
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import '@ag-grid-community/styles/ag-grid.css';
+import '@ag-grid-community/styles/ag-theme-alpine.css';
 
 // grid elements
 import { CustomOverlay, RowConfigType, GridHeader } from './data_table_header';
@@ -18,7 +18,6 @@ import { GridFooter } from './data_table_footer';
 // constants
 import { COLUMN_DEFAULT_MIN_WIDTH, HEADER_HEIGHT } from '../../../../../common/constants/explorer';
 import { IVisualizationContainerProps, IField } from '../../../../../common/types/explorer';
-import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 
 // styles
 import './data_table.scss';
