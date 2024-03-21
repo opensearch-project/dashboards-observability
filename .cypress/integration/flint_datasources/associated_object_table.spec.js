0d8d74c3839fae79ae938c2f7ecfdd530f7cfb70
Ryan Liang <jiallian@amazon.com>
2024-03-21 16:06:57 -0700

    Finish asso table header check 0  missing callout still
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- .cypress/integration/flint_datasources/associated_object_table.spec.js
+++ .cypress/integration/flint_datasources/associated_object_table.spec.js
@@ -5,51 +5,65 @@
 
 /// <reference types="cypress" />
 
-// ../../utils/flint-datasources/catalog-cache.json
-// ../../utils/flint-datasources/accelerations-cache.json
+import {
+	ASSO_TABLE_TITTLE,
+	ASSO_HEADER_DESC,
+	UPDATE_AT_DESC,
+	LOCALIZED_UPDATE_TIMESTAMP,
+	REFRESH_BTN_DESC,
+	CREATE_ACC_BTN_DESC,
+} from '../../utils/flint-datasources/panel_constants'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-  if (err.message.includes('ResizeObserver loop completed with undelivered notifications')) {
-    return false;
-  }
+	if (err.message.includes('ResizeObserver loop completed with undelivered notifications')) {
+		return false;
+	}
 });
 
 const goToDataConnectionPage = () => {
-  cy.visit(`${Cypress.env('opensearchDashboards')}/app/datasources`);
+	cy.visit(`${Cypress.env('opensearchDashboards')}/app/datasources`);
 	cy.get('h1[data-test-subj="dataconnections-header"]').should('be.visible');
 	cy.get('a[data-test-subj="mys3DataConnectionsLink"]').click();
 }
 
 describe('Associated Object test', () => {
-  before(() => {
-    // Load the catalog cache data
-    const catalogCachePath = './.cypress/utils/flint-datasources/catalog-cache.json';
-    cy.readFile(catalogCachePath).then((cache) => {
-      cy.visit(`${Cypress.env('opensearchDashboards')}`, {
-        onBeforeLoad: (win) => {
-          win.localStorage.setItem('async-query-catalog-cache', JSON.stringify(cache));
-        },
-      });
-    });
+	before(() => {
+		// Load the catalog cache data
+		const catalogCachePath = './.cypress/utils/flint-datasources/catalog-cache.json';
+		cy.readFile(catalogCachePath).then((cache) => {
+			cy.visit(`${Cypress.env('opensearchDashboards')}`, {
+				onBeforeLoad: (win) => {
+					win.localStorage.setItem('async-query-catalog-cache', JSON.stringify(cache));
+				},
+			});
+		});
 
-    // Load the accelerations cache data
-    const accelerationCachePath = './.cypress/utils/flint-datasources/accelerations-cache.json';
-    cy.readFile(accelerationCachePath).then((cache) => {
-      cy.visit(`${Cypress.env('opensearchDashboards')}`, {
-        onBeforeLoad: (win) => {
-          win.localStorage.setItem('async-query-acclerations-cache', JSON.stringify(cache));
-        },
-      });
-    });
-  });
+		// Load the accelerations cache data
+		const accelerationCachePath = './.cypress/utils/flint-datasources/accelerations-cache.json';
+		cy.readFile(accelerationCachePath).then((cache) => {
+			cy.visit(`${Cypress.env('opensearchDashboards')}`, {
+				onBeforeLoad: (win) => {
+					win.localStorage.setItem('async-query-acclerations-cache', JSON.stringify(cache));
+				},
+			});
+		});
+	});
 
 	after(() => {
 		cy.clearLocalStorage('async-query-catalog-cache');
 		cy.clearLocalStorage('async-query-acclerations-cache');
 	});
 
-  it('Navigates to data connection page and located the Associated objects table', () => {
-    goToDataConnectionPage();
-    cy.contains('h2.panel-title', 'Associated objects').should('exist');
-  });
+	it('Navigates to Associated objects table and check header elements', () => {
+		goToDataConnectionPage();
+
+		cy.contains('.euiFlexItem .euiText.euiText--medium', ASSO_TABLE_TITTLE)
+    .should('contain.text', ASSO_HEADER_DESC);
+
+		cy.contains('.euiTextColor--subdued', UPDATE_AT_DESC).should('exist');
+		cy.contains('.euiTextColor--subdued', LOCALIZED_UPDATE_TIMESTAMP).should('exist');
+
+		cy.contains('.euiButton--primary', REFRESH_BTN_DESC).should('exist');
+		cy.contains('.euiButton--primary.euiButton--fill', CREATE_ACC_BTN_DESC).should('exist');
+	});
 });
---
+++ .cypress/utils/flint-datasources/panel_constants.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const ASSO_TABLE_TITTLE = 'Associated objects';
+export const ASSO_HEADER_DESC = 'Manage objects associated with this data sources.';
+
+export const UPDATE_AT_DESC = 'Last updated at:';
+export const LOCALIZED_UPDATE_TIMESTAMP = '3/21/2024, 3:01:18 PM';
+
+export const REFRESH_BTN_DESC = 'Refresh';
+export const CREATE_ACC_BTN_DESC = 'Create acceleration';
