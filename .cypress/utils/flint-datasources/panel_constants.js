a5893e31c2596d6d42afdb8bb7751afe77e555eb
Ryan Liang <jiallian@amazon.com>
2024-03-22 15:52:43 -0700

    Refactor the constants file
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- .cypress/utils/flint-datasources/panel_constants.js
+++ .cypress/utils/flint-datasources/panel_constants.js
@@ -3,40 +3,50 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// Associated Object Table Callout
 export const ASSO_TABLE_CALLOUT = 'Accelerations recommended for tables. Setup acceleration or configure integrations';
 
+// Associated Object Table Header
 export const ASSO_TABLE_TITTLE = 'Associated objects';
 export const ASSO_HEADER_DESC = 'Manage objects associated with this data sources.';
-
 export const UPDATE_AT_DESC = 'Last updated at:';
 export const LOCALIZED_UPDATE_TIMESTAMP_AO = '3/21/2024, 3:01:18 PM';
 
 export const REFRESH_BTN_DESC = 'Refresh';
 export const CREATE_ACC_BTN_DESC = 'Create acceleration';
 
+// Associated Object Table Columns
 export const ASSO_NAME_COL = 'Name';
 export const ASSO_TYPE_COL = 'Type';
 export const ASSO_ACC_COL = 'Accelerations';
 export const ASSO_ACTION_COL = 'Actions';
 
+//Associated Object Table Filter
+export const ASSO_TABLE_FILTER = 'Accelerations';
+
+// Associated Object Table Search Bar
+export const ASSO_SEARCH_BAR_DEFAULT_INPUT = 'Search for objects';
+export const ASSO_SEARCH_BAR_FILTER_INPUT = 'accelerations:skipping_index';
+
+//Acceleration Table Header
+export const ACC_TABLE_TITLE = 'Accelerations';
+export const ACC_TABLE_DESC = 'Accelerations optimize query performance by indexing external data into OpenSearch.';
+
+export const LOCALIZED_UPDATE_TIMESTAMP_ACC = '3/21/2024, 3:01:22 PM';
+
+// Acceleration Table Columns
+export const ACC_NAME_COL = 'Name';
+export const ACC_TYPE_COL = 'Type';
+
+// Table Actions
+export const ACTION_ICON_DIS = 'Discover';
+export const ACTION_ICON_ACC = 'Accelerate';
+
+// Table Content
 export const DATABASE_NAME_DDEFAULT = 'default';
 export const TABLE_NAME_1 = 'http_logs';
 export const TABLE_NAME_2 = 'http_logs_1';
 export const TABLE_NAME_3 = 'table_no_timestamp';
 export const TABLE_NAME_4 = 'table_struct';
-
-export const ACTION_ICON_DIS = 'Discover';
-export const ACTION_ICON_ACC = 'Accelerate';
-
 export const AO_TYPE_TABLE = 'Table';
-
 export const SKIP_INDEX_NAME = 'skipping_index';
-
-export const ASSO_TABLE_FILTER = 'Accelerations';
-export const ASSO_SEARCH_BAR_DEFAULT_INPUT = 'Search for objects';
-export const ASSO_SEARCH_BAR_FILTER_INPUT = 'accelerations:skipping_index';
-
-export const ACC_TABLE_TITLE = 'Accelerations';
-export const ACC_TABLE_DESC = 'Accelerations optimize query performance by indexing external data into OpenSearch.';
-
-export const LOCALIZED_UPDATE_TIMESTAMP_ACC = '3/21/2024, 3:01:22 PM';
