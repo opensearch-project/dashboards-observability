b6b8117d29ac3d2846dccb79f0b2f5ffe9df0a14
Julian Quispel <jquispel@quintor.nl>
2024-05-02 21:54:35 +0200

    Replace fetch with http
    Signed-off-by: Julian Quispel <jquispel@quintor.nl>

--- public/components/trace_analytics/components/common/indices.ts
+++ public/components/trace_analytics/components/common/indices.ts
@@ -3,27 +3,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export async function loadTenantInfo(multitenancyEnabled: boolean) {
+import { CoreStart } from '../../../../../../../src/core/public';
+
+export async function loadTenantInfo(http: CoreStart['http'], multitenancyEnabled: boolean) {
   if (!multitenancyEnabled) {
     return;
   }
 
-  return await fetch(`../api/v1/multitenancy/tenant`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'osd-xsrf': 'true',
-      accept: '*/*',
-      'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6',
-      pragma: 'no-cache',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-origin',
-    },
-    method: 'GET',
-    referrerPolicy: 'strict-origin-when-cross-origin',
-    mode: 'cors',
-    credentials: 'include',
-  })
+  return await http
+    .get(`../api/v1/multitenancy/tenant`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'osd-xsrf': 'true',
+        accept: '*/*',
+        'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6',
+        pragma: 'no-cache',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+      },
+      referrerPolicy: 'strict-origin-when-cross-origin',
+      mode: 'cors',
+      credentials: 'include',
+    })
     .then((response) => {
       if (response.status === 404) {
         // endpoint doesn't exist, security plugin is not enabled.
