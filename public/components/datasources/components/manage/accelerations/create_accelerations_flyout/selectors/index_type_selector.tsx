5334641e3bf49064e51604207fd67e34ca0427a0
Amardeepsingh Siglani <amardeep7194@gmail.com>
2024-05-17 22:45:47 -0700

    don't show skipping index option in create acceleration flyout
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- public/components/datasources/components/manage/accelerations/create_accelerations_flyout/selectors/index_type_selector.tsx
+++ public/components/datasources/components/manage/accelerations/create_accelerations_flyout/selectors/index_type_selector.tsx
@@ -4,7 +4,7 @@
  */
 
 import { EuiFormRow, EuiLink, EuiSpacer, EuiSuperSelect, EuiText } from '@elastic/eui';
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState, useRef } from 'react';
 import {
   ACCELERATION_DEFUALT_SKIPPING_INDEX_NAME,
   ACC_INDEX_TYPE_DOCUMENTATION_URL,
@@ -16,16 +16,27 @@ import {
 
 interface IndexTypeSelectorProps {
   accelerationFormData: CreateAccelerationForm;
+  isS3ConnectionWithLakeFormation: boolean;
   setAccelerationFormData: React.Dispatch<React.SetStateAction<CreateAccelerationForm>>;
   initiateColumnLoad: (dataSource: string, database: string, dataTable: string) => void;
 }
 
 export const IndexTypeSelector = ({
   accelerationFormData,
+  isS3ConnectionWithLakeFormation,
   setAccelerationFormData,
   initiateColumnLoad,
 }: IndexTypeSelectorProps) => {
   const [value, setValue] = useState('skipping');
+  // This is used to track if user changed the value
+  // If so, we skip changing it based on 'isS3ConnectionWithLakeFormation' in the effect below
+  const valueSetAlready = useRef(false);
+
+  useEffect(() => {
+    if (!valueSetAlready.current) {
+      setValue(isS3ConnectionWithLakeFormation ? 'materialized' : 'skipping');
+    }
+  }, [isS3ConnectionWithLakeFormation]);
 
   useEffect(() => {
     initiateColumnLoad(
@@ -43,23 +54,30 @@ export const IndexTypeSelector = ({
         indexType === 'skipping' ? ACCELERATION_DEFUALT_SKIPPING_INDEX_NAME : '',
     });
     setValue(indexType);
+    valueSetAlready.current = true;
   };
 
+  const baseOptions = !isS3ConnectionWithLakeFormation
+    ? [
+        {
+          value: 'skipping',
+          inputDisplay: 'Skipping index',
+          dropdownDisplay: (
+            <Fragment>
+              <strong>Skipping index</strong>
+              <EuiText size="s" color="subdued">
+                <p className="EuiTextColor--subdued">
+                  Accelerate direct queries by storing table meta-data in OpenSearch.
+                </p>
+              </EuiText>
+            </Fragment>
+          ),
+        },
+      ]
+    : [];
+
   const superSelectOptions = [
-    {
-      value: 'skipping',
-      inputDisplay: 'Skipping index',
-      dropdownDisplay: (
-        <Fragment>
-          <strong>Skipping index</strong>
-          <EuiText size="s" color="subdued">
-            <p className="EuiTextColor--subdued">
-              Accelerate direct queries by storing table meta-data in OpenSearch.
-            </p>
-          </EuiText>
-        </Fragment>
-      ),
-    },
+    ...baseOptions,
     {
       value: 'covering',
       inputDisplay: 'Covering index',
