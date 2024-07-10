f27163df64a75161880837f4415ae6b3003be7c5
sumukhswamy <sumukhhs@amazon.com>
2024-07-10 16:33:41 -0700

    updated tests
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- server/adaptors/notebooks/saved_objects_notebooks_router.tsx
+++ server/adaptors/notebooks/saved_objects_notebooks_router.tsx
@@ -80,7 +80,6 @@ export async function addSampleNotes(
         dateModified: createdNotebooks.attributes.savedNotebook.dateModified,
         name: createdNotebooks.attributes.savedNotebook.name,
         id: createdNotebooks.id,
-        // name: createdNotebooks.attributes.savedNotebook.name
       });
     }
 
