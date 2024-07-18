ef393c7b3bde3c0372ee23800e0ac8c8aeac6552
Adam Tackett <105462877+TackAdam@users.noreply.github.com>
2024-07-17 22:28:39 -0700

    Update public/plugin_nav.tsx
    Co-authored-by: SuZhou-Joe <suzhou@amazon.com>
    Signed-off-by: Adam Tackett <105462877+TackAdam@users.noreply.github.com>

--- public/plugin_nav.tsx
+++ public/plugin_nav.tsx
@@ -47,6 +47,7 @@ export function registerAllPluginNavGroups(core: CoreSetup<AppPluginStartDepende
     {
       id: observabilityNotebookID,
       category: DEFAULT_APP_CATEGORIES.dashboardAndReport,
+      order: 400,
     },
   ]);
 
