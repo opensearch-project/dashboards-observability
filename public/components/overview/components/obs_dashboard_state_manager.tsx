c716c503857a0975f5e85becf3c434fe129a0b6c
Shenoy Pratik <sgguruda@amazon.com>
2024-09-03 11:20:59 -0700

    add comment to move away from rxjs
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- public/components/overview/components/obs_dashboard_state_manager.tsx
+++ public/components/overview/components/obs_dashboard_state_manager.tsx
@@ -6,6 +6,11 @@
 import { BehaviorSubject } from 'rxjs';
 import { DashboardState } from '../../../../common/types/overview';
 
+/**
+ * Class to manage the state of the observability dashboard.
+ * TODO: Change this to redux or react context provider.
+ *       RXJS is a temporary solution here
+ */
 export class ObsDashboardStateManager {
   static isDashboardSelected$ = new BehaviorSubject<boolean>(false);
   static dashboardState$ = new BehaviorSubject<DashboardState>({
