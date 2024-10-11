f68eba1c45920df2866875dd5542cfe805e3f241
Adam Tackett <105462877+TackAdam@users.noreply.github.com>
2024-10-11 16:29:58 -0700

    GettingStarted Rework (#2194)
    * UI pass updating everything
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * dashboard re-direct to integration for otel
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * correct naming and icon for three cases
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * updates to jsons and ndjsons
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * remove dashboard creation from the create assets call
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * update name to Self Managed
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * Remove hyperlink from discover
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * address comments
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    ---------
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    Co-authored-by: Adam Tackett <tackadam@amazon.com>

--- server/routes/getting_started/helper.ts
+++ server/routes/getting_started/helper.ts
@@ -5,35 +5,28 @@
 
 import fs from 'fs';
 import path from 'path';
+import {
+  COMPONENT_MAP,
+  VERSION_MAP,
+  SIGNAL_MAP,
+  TutorialId,
+} from '../../../common/constants/getting_started_routes';
 
-export const assetMapper = (tutorialId: string) => {
-  switch (tutorialId) {
-    case 'otel':
-      return 'otel-index-patterns-1.0.0.ndjson';
-    case 'nginx':
-      return 'nginx-1.0.0.ndjson';
-    case 'java':
-      return 'java-tutorial-1.0.0.ndjson';
-    case 'python':
-      return 'python-tutorial-1.0.0.ndjson';
-    case 'golang':
-      return 'golang-tutorial-1.0.0.ndjson';
-    case 'csv':
-      return 'fluent-bit-csv-upload-1.0.0.ndjson';
-    case 'data-prepper':
-      return 'dummy.ndjson';
-    default:
-      return '';
-  }
+export const assetMapper = (tutorialId: TutorialId): string => {
+  const component = COMPONENT_MAP[tutorialId] || 'default-component';
+  const version = VERSION_MAP[tutorialId] || '1.0.0';
+  const signal = SIGNAL_MAP[tutorialId] ? `-${SIGNAL_MAP[tutorialId]}` : '';
+
+  return `${component}-${version}${signal}.ndjson`;
 };
 
-export const loadAssetsFromFile = async (tutorialId: string) => {
+export const loadAssetsFromFile = async (tutorialId: TutorialId) => {
   try {
     const fileName = assetMapper(tutorialId);
     const filePath = path.join(__dirname, `assets/${fileName}`);
     const fileData = await fs.promises.readFile(filePath, 'utf8');
     return fileData;
   } catch (error) {
-    throw new Error('Issue is loading asset');
+    throw new Error(`Error loading asset: ${tutorialId}`);
   }
 };
