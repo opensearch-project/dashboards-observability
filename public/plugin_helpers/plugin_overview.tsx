90a2baffa35c8297da0cc16dd4841393e90058c6
Adam Tackett <105462877+TackAdam@users.noreply.github.com>
2024-10-18 13:05:50 -0700

    Observability Overview page rework (#2210)
    * rework the obs overview page adding dismiss to cards, and changing ui
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * hide new setting for showCards based on workspace
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * small integrations ui adjustment
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * Update snapshots
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * fix lint
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * capitalize Page
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    ---------
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    Signed-off-by: Adam Tackett <105462877+TackAdam@users.noreply.github.com>
    Co-authored-by: Adam Tackett <tackadam@amazon.com>

--- public/plugin_helpers/plugin_overview.tsx
+++ public/plugin_helpers/plugin_overview.tsx
@@ -4,7 +4,10 @@
  */
 
 import React from 'react';
-import { ContentManagementPluginSetup } from '../../../../src/plugins/content_management/public';
+import {
+  ContentManagementPluginSetup,
+  Section,
+} from '../../../../src/plugins/content_management/public';
 
 export const HOME_PAGE_ID = 'observabilityOverviewPage';
 
@@ -20,19 +23,20 @@ export enum HOME_CONTENT_AREAS {
   DASHBOARD = `${HOME_PAGE_ID}/${SECTIONS.DASHBOARD}`,
 }
 
+export const GET_STARTED_SECTION: Section = {
+  id: SECTIONS.GET_STARTED,
+  order: 1000,
+  title: '',
+  kind: 'card',
+  wrap: false,
+  columns: 5,
+};
+
 export const setupOverviewPage = (contentManagement: ContentManagementPluginSetup) => {
-  contentManagement.registerPage({
+  return contentManagement.registerPage({
     id: HOME_PAGE_ID,
     title: 'Home',
     sections: [
-      {
-        id: SECTIONS.GET_STARTED,
-        order: 1000,
-        title: 'Get started',
-        kind: 'card',
-        wrap: false,
-        columns: 5,
-      },
       {
         id: SECTIONS.SELECTOR,
         order: 2000,
