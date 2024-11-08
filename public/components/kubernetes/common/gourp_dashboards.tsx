059931819263a7c3ac34451654270e0ff2230867
Eric <menwe@amazon.com>
2024-11-08 10:31:23 +0000

    kubernetes dashboards initial visualizations
    Signed-off-by: Eric <menwe@amazon.com>

--- public/components/kubernetes/common/gourp_dashboards.tsx
+++ public/components/kubernetes/common/gourp_dashboards.tsx
@@ -3,194 +3,311 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
-import { EuiPanel, EuiText, EuiFlexItem } from '@elastic/eui';
+import React, { useEffect } from 'react';
+import {
+  EuiPanel,
+  EuiFlexItem,
+  EuiFlexGroup,
+  EuiCompressedSuperDatePicker,
+  EuiSpacer,
+  EuiPageHeader,
+} from '@elastic/eui';
 import { Responsive, WidthProvider } from 'react-grid-layout';
-// import MetricCard from '../common/MetricCard';
-// import TimeSeriesChart from '../common/TimeSeriesChart';
+import { PlotlyHTMLElement } from 'plotly.js';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import PPLService from '../../../services/requests/ppl';
+import { coreRefs } from '../../../framework/core_refs';
+import { uiSettingsService } from '../../../../common/utils';
+import { Plt } from '../../../components/visualizations/plotly/plot';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
-export const GroupDashboards = () => {
-  // Define layouts for different breakpoints
-  const layouts = {
-    lg: [
-      { i: 'globalCPUUsage', x: 0, y: 0, w: 2, h: 1 },
-      { i: 'globalRAMUsage', x: 2, y: 0, w: 2, h: 1 },
-      { i: 'nodes', x: 4, y: 0, w: 1, h: 1 },
-      { i: 'namespaces', x: 5, y: 0, w: 1, h: 1 },
-      { i: 'runningPods', x: 6, y: 0, w: 1, h: 1 },
+interface IPrometheusQuery {
+  cluster: string;
+  name: string;
+  title: string;
+  query: string;
+  endpoint: string;
+  vis: { component: PlotlyHTMLElement };
+}
 
-      { i: 'clusterCPUUtilization', x: 0, y: 1, w: 3, h: 1 },
-      { i: 'clusterMemoryUtilization', x: 3, y: 1, w: 4, h: 1 },
+export interface GroupDashboardsProps {
+  prometheusQueries: IPrometheusQuery[];
+}
 
-      { i: 'cpuUsage', x: 0, y: 2, w: 3, h: 1 },
-      { i: 'ramUsage', x: 3, y: 2, w: 4, h: 1 },
+export interface ISchema {
+  name: string;
+  type: string;
+}
+export interface IMetrics {
+  datarows: any[];
+  jsonData: { [key: string]: any };
+  schema: ISchema[];
+}
 
-      { i: 'networkTraffic', x: 0, y: 3, w: 4, h: 2 },
-      { i: 'diskIO', x: 4, y: 3, w: 3, h: 2 },
+const pplService = new PPLService(coreRefs.http);
 
-      { i: 'cpuUsageByNamespace', x: 0, y: 5, w: 4, h: 2 },
-      { i: 'memoryUsageByNamespace', x: 4, y: 5, w: 3, h: 2 },
-    ],
-    md: [
-      { i: 'globalCPUUsage', x: 0, y: 0, w: 2, h: 1 },
-      { i: 'globalRAMUsage', x: 2, y: 0, w: 2, h: 1 },
-      { i: 'nodes', x: 4, y: 0, w: 1, h: 1 },
-      { i: 'namespaces', x: 5, y: 0, w: 1, h: 1 },
-      { i: 'runningPods', x: 6, y: 0, w: 1, h: 1 },
+const layouts = {
+  lg: [
+    // Top row with global usage and counts
+    { i: 'clusterCPUUsage', x: 0, y: 0, w: 5, h: 2 },
+    { i: 'clusterMemoryUsage', x: 2, y: 0, w: 2, h: 2 },
+    { i: 'nodesCount', x: 4, y: 0, w: 1, h: 1 },
+    { i: 'namespacesCount', x: 5, y: 0, w: 1, h: 2 },
+    { i: 'runningPodsCount', x: 6, y: 0, w: 2, h: 2 },
 
-      { i: 'clusterCPUUtilization', x: 0, y: 1, w: 3, h: 1 },
-      { i: 'clusterMemoryUtilization', x: 3, y: 1, w: 4, h: 1 },
+    // Middle row with CPU and RAM utilization
+    // { i: 'cpuUsage', x: 0, y: 2, w: 2, h: 1 },
+    // { i: 'ramUsage', x: 2, y: 2, w: 2, h: 1 },
+    // { i: 'clusterCPUUtilization', x: 4, y: 2, w: 2, h: 1 },
+    // { i: 'clusterMemoryUtilization', x: 6, y: 2, w: 2, h: 1 },
 
-      { i: 'cpuUsage', x: 0, y: 2, w: 3, h: 1 },
-      { i: 'ramUsage', x: 3, y: 2, w: 4, h: 1 },
+    // Additional information panels
+    { i: 'runningPodsCategory', x: 0, y: 3, w: 3, h: 2 },
+    { i: 'totalNetworkTraffic', x: 3, y: 3, w: 5, h: 2 },
 
-      { i: 'networkTraffic', x: 0, y: 3, w: 4, h: 2 },
-      { i: 'diskIO', x: 4, y: 3, w: 3, h: 2 },
+    // Visualization row with detailed charts
+    // { i: 'networkTraffic', x: 0, y: 5, w: 4, h: 3 },
+    // { i: 'diskIO', x: 4, y: 5, w: 4, h: 3 },
 
-      { i: 'cpuUsageByNamespace', x: 0, y: 5, w: 4, h: 2 },
-      { i: 'memoryUsageByNamespace', x: 4, y: 5, w: 3, h: 2 },
-    ],
-    sm: [
-      { i: 'globalCPUUsage', x: 0, y: 0, w: 2, h: 1 },
-      { i: 'globalRAMUsage', x: 0, y: 1, w: 2, h: 1 },
-      { i: 'nodes', x: 0, y: 2, w: 1, h: 1 },
-      { i: 'namespaces', x: 1, y: 2, w: 1, h: 1 },
-      { i: 'runningPods', x: 0, y: 3, w: 2, h: 1 },
+    // Namespace-based CPU and memory usage panels
+    { i: 'cpuUsageByNamespace', x: 0, y: 8, w: 4, h: 3 },
+    { i: 'memoryUsageByNamespace', x: 4, y: 8, w: 4, h: 3 },
 
-      { i: 'clusterCPUUtilization', x: 0, y: 4, w: 2, h: 1 },
-      { i: 'clusterMemoryUtilization', x: 0, y: 5, w: 2, h: 1 },
+    // Final row with additional utilization details
+    { i: 'podNetworkUtilization', x: 0, y: 10, w: 4, h: 3 },
+  ],
+  md: [
+    // Adjustments for medium-sized screens
+    { i: 'clusterCPUUsage', x: 0, y: 0, w: 2, h: 2 },
+    { i: 'clusterMemoryUsage', x: 2, y: 0, w: 2, h: 2 },
+    { i: 'nodesCount', x: 4, y: 0, w: 1, h: 2 },
+    { i: 'namespacesCount', x: 5, y: 0, w: 1, h: 2 },
+    { i: 'runningPodsCount', x: 6, y: 0, w: 2, h: 2 },
+    { i: 'cpuUsage', x: 0, y: 2, w: 2, h: 1 },
+    { i: 'ramUsage', x: 2, y: 2, w: 2, h: 1 },
+    { i: 'clusterCPUUtilization', x: 4, y: 2, w: 2, h: 1 },
+    { i: 'clusterMemoryUtilization', x: 6, y: 2, w: 2, h: 1 },
+    { i: 'runningPodsCategory', x: 0, y: 3, w: 3, h: 2 },
+    { i: 'totalNetworkTraffic', x: 3, y: 3, w: 5, h: 2 },
+    { i: 'networkTraffic', x: 0, y: 5, w: 4, h: 3 },
+    { i: 'diskIO', x: 4, y: 5, w: 4, h: 3 },
+    { i: 'cpuUsageByNamespace', x: 0, y: 8, w: 4, h: 3 },
+    { i: 'memoryUsageByNamespace', x: 4, y: 8, w: 4, h: 3 },
+    { i: 'podNetworkUtilization', x: 0, y: 11, w: 4, h: 3 },
+  ],
+  sm: [
+    // Adjustments for small screens
+    { i: 'clusterCPUUsage', x: 0, y: 0, w: 2, h: 2 },
+    { i: 'clusterMemoryUsage', x: 2, y: 0, w: 2, h: 2 },
+    { i: 'nodesCount', x: 0, y: 2, w: 2, h: 2 },
+    { i: 'namespacesCount', x: 2, y: 2, w: 2, h: 2 },
+    { i: 'runningPodsCount', x: 0, y: 4, w: 2, h: 2 },
+    { i: 'cpuUsage', x: 2, y: 4, w: 2, h: 1 },
+    { i: 'ramUsage', x: 0, y: 5, w: 2, h: 1 },
+    { i: 'clusterCPUUtilization', x: 2, y: 5, w: 2, h: 1 },
+    { i: 'clusterMemoryUtilization', x: 0, y: 6, w: 2, h: 1 },
+    { i: 'runningPodsCategory', x: 2, y: 6, w: 2, h: 2 },
+    { i: 'totalNetworkTraffic', x: 0, y: 7, w: 4, h: 3 },
+    { i: 'networkTraffic', x: 0, y: 10, w: 4, h: 3 },
+    { i: 'diskIO', x: 0, y: 13, w: 4, h: 3 },
+    { i: 'cpuUsageByNamespace', x: 0, y: 16, w: 4, h: 3 },
+    { i: 'memoryUsageByNamespace', x: 0, y: 19, w: 4, h: 3 },
+    { i: 'podNetworkUtilization', x: 0, y: 22, w: 4, h: 3 },
+  ],
+};
 
-      { i: 'cpuUsage', x: 0, y: 6, w: 2, h: 1 },
-      { i: 'ramUsage', x: 0, y: 7, w: 2, h: 1 },
+export const GroupDashboards = ({
+  prometheusQueries,
+  setStart,
+  setEnd,
+  start,
+  end,
+}: GroupDashboardsProps) => {
+  const [metricsData, setMetricsData] = React.useState<any[]>([]);
 
-      { i: 'networkTraffic', x: 0, y: 8, w: 2, h: 2 },
-      { i: 'diskIO', x: 0, y: 10, w: 2, h: 2 },
+  useEffect(() => {
+    console.log('reredner on date change.....');
+    const metrics = prometheusQueries.map((query) => {
+      return pplService.fetch({ query: query.query, format: 'viz' });
+    });
 
-      { i: 'cpuUsageByNamespace', x: 0, y: 12, w: 2, h: 2 },
-      { i: 'memoryUsageByNamespace', x: 0, y: 14, w: 2, h: 2 },
-    ],
+    Promise.allSettled(metrics)
+      .then((data) => {
+        setMetricsData(data.map((d) => (d.status === 'fulfilled' ? d.value : null)));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [start, end]);
+
+  const getProcessedMetricsVizData = (
+    data: IMetrics,
+    xKeys: string[],
+    yKeys: string[],
+    gKeys: string[],
+    traceConfig: {}
+  ) => {
+    console.log('data: ', data);
+    console.log('xKeys[0]: ', xKeys[0]);
+    console.log('yKeys[0]: ', yKeys[0]);
+    if (gKeys.length === 0) {
+      return [
+        {
+          x: data.data[xKeys[0]],
+          y: data.data[yKeys[0]],
+          ...traceConfig,
+        },
+      ];
+    }
+    const groupMap = new Map<string, any>();
+    data.jsonData.forEach((row) => {
+      const groupKey = gKeys.map((gKey) => row[gKey]).join(',');
+      if (!groupMap.has(groupKey)) {
+        groupMap.set(groupKey, {
+          x: [row[xKeys[0]]],
+          y: [row[yKeys[0]]],
+          name: groupKey === ',' ? yKeys[0] : groupKey,
+          ...traceConfig,
+        });
+      } else {
+        const group = groupMap.get(groupKey);
+        group.x.push(row[xKeys[0]]);
+        group.y.push(row[yKeys[0]]);
+      }
+    });
+    return [...groupMap.values()];
+
+    // data.jsonData.forEach((pair) => {
+
+    // });
+  };
+
+  const renderPanel = (metrics: IMetrics[]) => {
+    console.log('metrics', metrics);
+    return metrics.map((metric, index) => {
+      if (!metric) {
+        return null;
+      }
+
+      const visXaxisKeys = prometheusQueries[index].vis.xKeys;
+      const visYaxisKeys = prometheusQueries[index].vis.yKeys;
+      const visGroupingKeys = prometheusQueries[index].vis.gKeys;
+      console.log('metric: ', metric);
+      const visualizationData = getProcessedMetricsVizData(
+        metric,
+        visXaxisKeys,
+        visYaxisKeys,
+        visGroupingKeys,
+        prometheusQueries[index].vis.config
+      );
+      console.log('visualizationData: ', visualizationData);
+
+      // metric
+      // jdbc
+      // metric.datarows.forEach((row) => {
+      //   xaxisData.push(row[0]);
+      //   yaxisData.push(row[1]);
+      //   // row[0] = new Date(row[0]).toISOString();
+      // });
+      return (
+        <div key={prometheusQueries[index].title + index} data-grid={layouts.lg[index]}>
+          <EuiPanel paddingSize="m">
+            <Plt
+              // data={[
+              //   {
+              //     x: xaxisData,
+              //     y: yaxisData,
+              //     type: prometheusQueries[index].vis?.type || 'scatter',
+              //   },
+              // ]}
+              data={visualizationData}
+              layout={{
+                showlegend: true,
+                ...(prometheusQueries[index].vis.layout || {}),
+                xaxis: {
+                  // title: {
+                  //   text: prometheusQueries[index].vis.x || metric.schema[0]?.name || '',
+                  // },
+                  showgrid: true,
+                  zeroline: false,
+                  rangemode: 'normal',
+                  automargin: true,
+                },
+                yaxis: {
+                  title: {
+                    text: prometheusQueries[index].vis.y || metric.schema[1]?.name || '',
+                  },
+                  showgrid: true,
+                  zeroline: false,
+                  rangemode: 'normal',
+                },
+              }}
+              // config={prometheusQueries[index].vis.component.config}
+            />
+            <EuiSpacer size="m" />
+            {/* <Visualization
+            visualizations={{
+              data: {
+                userConfigs: {
+                  dataConfig: {
+                    span: {},
+                    [GROUPBY]: metric.schema[1]?.name,
+                    [AGGREGATIONS]: metric.schema[0]?.name,
+                    queryMetaData: {},
+                    metricType: '',
+                  },
+                },
+                explorer: {
+                  explorerData: { schema: metric.schema, jsonData: metric.jsonData },
+                },
+              },
+              vis: {
+                component: prometheusQueries[index].vis.component,
+              },
+            }}
+          /> */}
+          </EuiPanel>
+        </div>
+      );
+    });
   };
 
   return (
-    <ResponsiveGridLayout
-      className="layout"
-      layouts={layouts}
-      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-      cols={{ lg: 7, md: 7, sm: 2, xs: 1, xxs: 1 }}
-      rowHeight={200} // Increased row height for more vertical space
-      isDraggable={true}
-      isResizable={true}
-      width={window.innerWidth}
-    >
-      {/* Define each panel as in previous code with adjusted layout keys */}
-
-      <EuiFlexItem key="globalCPUUsage">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Global CPU Usage</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="globalRAMUsage">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Global RAM Usage</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="nodes">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Nodes</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="namespaces">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Namespaces</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="runningPods">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Running Pods</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="clusterCPUUtilization">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Cluster CPU Utilization</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="clusterMemoryUtilization">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Cluster Memory Utilization</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="cpuUsage">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>CPU Usage</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="ramUsage">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>RAM Usage</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="networkTraffic">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Total Network Traffic</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="diskIO">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Disk I/O</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="cpuUsageByNamespace">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>CPU Utilization by Namespace</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-
-      <EuiFlexItem key="memoryUsageByNamespace">
-        <EuiPanel paddingSize="m">
-          <EuiText>
-            <h3>Memory Utilization by Namespace</h3>
-          </EuiText>
-        </EuiPanel>
-      </EuiFlexItem>
-    </ResponsiveGridLayout>
+    <div className="group-dashboards">
+      <EuiFlexGroup direction="column">
+        <EuiFlexGroup direction="row" className="kubernetes-cluster-title">
+          <EuiFlexItem>
+            <EuiPageHeader pageTitle={'eks-cluster-with-vpc'} />
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiCompressedSuperDatePicker
+              dateFormat={uiSettingsService.get('dateFormat')}
+              start={start}
+              end={end}
+              onTimeChange={(dateSpan) => {
+                setStart(dateSpan.start);
+                setEnd(dateSpan.end);
+              }}
+              showUpdateButton={false}
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiSpacer size="m" />
+        <ResponsiveGridLayout
+          className="layout"
+          layouts={layouts}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 8, md: 8, sm: 4, xs: 2, xxs: 1 }}
+          rowHeight={250} // Adjust as needed for more vertical space
+          isDraggable={true}
+          isResizable={true}
+          width={window.innerWidth}
+        >
+          {renderPanel(metricsData)}
+        </ResponsiveGridLayout>
+      </EuiFlexGroup>
+    </div>
   );
 };
--- public/components/kubernetes/home.tsx
+++ public/components/kubernetes/home.tsx
@@ -5,7 +5,179 @@
 
 import React from 'react';
 import { GroupDashboards } from './common/gourp_dashboards';
+import { Line } from '../visualizations/charts/lines/line';
+import { convertDateTime } from '../common/query_utils';
+
+// function determineExactSpan(startTime: number, endTime: number): string {
+//   const durationInSeconds = Math.round(
+//     (new Date(endTime).getTime() - new Date(startTime).getTime()) / 1000
+//   ); // Round to nearest second
+
+//   // Convert the duration into the appropriate PPL-compatible span format
+//   if (durationInSeconds < 60) {
+//     return `${durationInSeconds}s`; // Duration in seconds
+//   } else if (durationInSeconds < 3600) {
+//     return `${Math.floor(durationInSeconds / 60)}m`; // Duration in minutes
+//   } else if (durationInSeconds < 86400) {
+//     return `${Math.floor(durationInSeconds / 3600)}h`; // Duration in hours
+//   } else if (durationInSeconds < 604800) {
+//     return `${Math.floor(durationInSeconds / 86400)}d`; // Duration in days
+//   } else if (durationInSeconds < 2592000) {
+//     return `${Math.floor(durationInSeconds / 604800)}w`; // Duration in weeks
+//   } else if (durationInSeconds < 31536000) {
+//     return `${Math.floor(durationInSeconds / 2592000)}M`; // Duration in months
+//   } else {
+//     return `${Math.floor(durationInSeconds / 31536000)}y`; // Duration in years
+//   }
+// }
+
+function getSpanFromTimeRange(startTime, endTime) {
+  const durationInSeconds = new Date(endTime).getTime() - new Date(startTime).getTime();
+
+  // Map specific time ranges to spans
+  if (durationInSeconds < 3600 * 1000) {
+    return '10s'; // Less than 1 hour, use seconds
+  } else if (durationInSeconds < 24 * 3600 * 1000) {
+    return '15m'; // Less than 1 day, use 15 minutes
+  } else if (durationInSeconds < 7 * 24 * 3600 * 1000) {
+    return '1h'; // Less than 1 week, use 1 hour
+  } else {
+    return '1d'; // More than 1 week, use 1 day
+  }
+}
 
 export const Home = () => {
-  return <GroupDashboards metricsData={{}} />;
+  const [startTime, setStart] = React.useState<string>('now-15m');
+  const [endTime, setEnd] = React.useState<string>('now');
+
+  const start = convertDateTime(startTime, true);
+  const end = convertDateTime(endTime, false);
+  // const exactTimeInterval = determineExactSpan(start, end);
+  const timeInterval = getSpanFromTimeRange(start, end);
+
+  return (
+    <GroupDashboards
+      setStart={setStart}
+      setEnd={setEnd}
+      start={startTime}
+      end={endTime}
+      prometheusQueries={[
+        {
+          cluster: 'eks-cluster-with-vpc',
+          name: 'clusterCPUUsage',
+          title: 'Cluster CPU Usage',
+          // query: `source = prometheus_k8s_cluster.\`cluster:node_cpu:sum_rate5m\` | where @timestamp >= '${start}' and @timestamp <= '${end}'`,
+          query: `source = prometheus_k8s_cluster.container_cpu_usage_seconds_total | where @timestamp >= '${start}' and @timestamp <= '${end}' | where \`cluster\` = 'eks-cluster-with-vpc' | stats sum(@value) as total_cpu_usage by span(@timestamp,${timeInterval})`,
+          endpoint: 'prometheus',
+          datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+          vis: {
+            component: Line,
+            x: 'timestamp',
+            y: 'total_cpu_usage',
+            xaxisKey: '@timestamp',
+            yaxisKey: 'total_cpu_usage',
+            config: {
+              type: 'line',
+              mode: 'lines',
+              line: {
+                shape: 'spline', // This makes the line smooth
+              },
+            },
+            xKeys: [`span(@timestamp,${timeInterval})`],
+            yKeys: ['total_cpu_usage'],
+            gKeys: [],
+          },
+        },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'clusterMemoryUsage',
+        //   title: 'Cluster Memory',
+        //   query: `source = prometheus_k8s.\`count:up1\` | stats count() as running_node_count by span(@timestamp, ${timeInterval})`,
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'nodesCount',
+        //   title: 'Nodes',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'namespacesCount',
+        //   title: 'Namespaces',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Pie },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'runningPodsCount',
+        //   title: 'Running Pods',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'runningPodsCategory',
+        //   title: 'Running Pods',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'totalNetworkTraffic',
+        //   title: 'Total Network Traffic',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'cpuUsageByNamespace',
+        //   title: 'CPU Utilization by Namespace',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'memoryUsageByNamespace',
+        //   title: 'Memory Utilization by Namespace',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+        // {
+        //   cluster: 'play-db-cluster',
+        //   name: 'podNetworkUtilization',
+        //   title: 'Pod Network Utilization',
+        //   query:
+        //     'source = prometheus_k8s.node_cpu_seconds_total | stats avg(@value) by span(@timestamp, 1m)',
+        //   endpoint: 'prometheus',
+        //   datasource: { name: 'prometheus_k8s', type: 'prometheus' },
+        //   vis: { component: Line },
+        // },
+      ]}
+    />
+  );
 };
