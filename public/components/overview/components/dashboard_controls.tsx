3631968e9fa7042287822ac2830810c4b7da28d5
Adam Tackett <105462877+TackAdam@users.noreply.github.com>
2024-11-12 09:39:48 -0800

    Overview page add state for missing data source (#2237)
    * added callout for no data sources pathway in workspaces
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * reset dashboard if datasource is deleted
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * handle refresh, adjust naming, unit test
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * add i18n, finally block, move function
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    ---------
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    Co-authored-by: Adam Tackett <tackadam@amazon.com>

--- public/components/overview/components/dashboard_controls.tsx
+++ public/components/overview/components/dashboard_controls.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import React, { useEffect, useState } from 'react';
 import {
   EuiFlexGroup,
   EuiFlexItem,
@@ -13,18 +14,58 @@ import {
   EuiToolTip,
 } from '@elastic/eui';
 import { OnTimeChangeProps } from '@opensearch-project/oui/src/eui_components/date_picker/super_date_picker/super_date_picker';
-import React from 'react';
 import { useObservable } from 'react-use';
 import { FormattedMessage } from '@osd/i18n/react';
 import { coreRefs } from '../../../framework/core_refs';
-import { HOME_CONTENT_AREAS } from '../../../plugin_helpers/plugin_overview';
+import { HOME_CONTENT_AREAS, SECTIONS } from '../../../plugin_helpers/plugin_overview';
 import { redirectToDashboards } from '../../getting_started/components/utils';
 import { AddDashboardCallout } from './add_dashboard_callout';
+import { AddDataSourceCallout } from './add_datasource_callout';
 import { ObsDashboardStateManager } from './obs_dashboard_state_manager';
+import { SavedObjectsClientCommonFindArgs } from '../../../../../../src/plugins/data/common';
+import { getWorkspaceIdFromUrl } from '../../../../../../src/core/public/utils';
+import { setObservabilityDashboardsId } from './utils';
+import { getOverviewPage } from '../../../../common/utils';
+
+const getDatasourceAttributes = async () => {
+  const findOptions: SavedObjectsClientCommonFindArgs = {
+    type: 'data-source',
+    perPage: 1000,
+  };
+
+  const allDataSources = await coreRefs?.savedObjectsClient?.find(findOptions);
+  return allDataSources?.savedObjects ?? [];
+};
 
 export function DashboardControls() {
+  const [isDataSourceEmpty, setIsDataSourceEmpty] = useState<boolean | null>(null);
+  const [isInWorkspace, setIsInWorkspace] = useState<boolean>(false); // Track if user is in a workspace
   const isDashboardSelected = useObservable(ObsDashboardStateManager.isDashboardSelected$);
   const dashboardState = useObservable(ObsDashboardStateManager.dashboardState$);
+
+  const checkDataSource = async () => {
+    const currentUrl = window.location.href;
+    const workspaceId = getWorkspaceIdFromUrl(currentUrl, coreRefs?.http?.basePath.getBasePath());
+
+    if (workspaceId) {
+      setIsInWorkspace(true);
+      const savedObjectsArray = await getDatasourceAttributes();
+      setIsDataSourceEmpty(savedObjectsArray.length === 0);
+
+      // Set to null if there are no data sources associated [Handle if dashboard was set, then datasource deleted]
+      if (savedObjectsArray.length === 0) {
+        await setObservabilityDashboardsId(null);
+        getOverviewPage().removeSection(SECTIONS.DASHBOARD); // Clear the present dashboard
+      }
+    } else {
+      setIsInWorkspace(false);
+      setIsDataSourceEmpty(false); // Not in workspace
+    }
+  };
+
+  useEffect(() => {
+    checkDataSource();
+  }, []);
 
   const onTimeChange = (onTimeChangeProps: OnTimeChangeProps) => {
     ObsDashboardStateManager.dashboardState$.next({
@@ -46,6 +87,11 @@ export function DashboardControls() {
       return section;
     });
   };
+
+  // Directly show AddDataSourceCallout if in workspace and no data source is associated
+  if (isInWorkspace && isDataSourceEmpty) {
+    return <AddDataSourceCallout />;
+  }
 
   return isDashboardSelected ? (
     <EuiFlexGroup gutterSize="s" alignItems="center" justifyContent="spaceBetween">
