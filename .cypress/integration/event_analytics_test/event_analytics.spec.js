27586d47d7518717d583b129e6cd071a94b46a75
Adam Tackett <tackadam@amazon.com>
2025-01-31 16:09:13 -0800

    fix flaky cypress test for event_analyics
    Signed-off-by: Adam Tackett <tackadam@amazon.com>

--- .cypress/integration/event_analytics_test/event_analytics.spec.js
+++ .cypress/integration/event_analytics_test/event_analytics.spec.js
@@ -281,8 +281,12 @@ describe('Override timestamp for an index', () => {
     clearQuerySearchBoxText('searchAutocompleteTextArea');
     cy.get('[data-test-subj="searchAutocompleteTextArea"]').type(TEST_QUERIES[2].query);
     cy.get('[data-test-subj="superDatePickerApplyTimeButton"]').click();
+    cy.get('[data-test-subj="globalLoadingIndicator"]').should('not.exist');
     cy.get('.tab-title').contains('Events').click();
-    cy.get('[data-test-subj="eventExplorer__overrideDefaultTimestamp"]').click({ force: true });
+    cy.get('[data-test-subj="globalLoadingIndicator"]').should('not.exist');
+    cy.get('[data-test-subj="eventExplorer__overrideDefaultTimestamp"]')
+    .first()
+    .click({ force: true });
 
     cy.get('[data-attr-field="utc_time"] [data-test-subj="eventFields__default-timestamp-mark"')
       .contains('Default Timestamp')
