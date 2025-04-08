9d67b0b37783f5513a84b6710a7e752277f0b9f6
Adam Tackett <tackadam@amazon.com>
2025-04-08 14:37:38 -0700

    test
    Signed-off-by: Adam Tackett <tackadam@amazon.com>

--- .cypress/support/index.js
+++ .cypress/support/index.js
@@ -23,8 +23,6 @@ import 'cypress-watch-and-reload/support'
 // Import commands.js using ES2015 syntax:
 import './commands';
 
-import ResizeObserverPolyfill from 'resize-observer-polyfill';
-
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
@@ -33,7 +31,46 @@ if (Cypress.env('security_enabled')) {
   Cypress.env('opensearch', 'https://localhost:9200');
 }
 
-// Fix for electron crash https://github.com/cypress-io/cypress/issues/27415#issuecomment-2594288913
+// Fix for ResizeObserver crash in Electron
+// https://github.com/cypress-io/cypress/issues/27415#issuecomment-2594288913
 Cypress.on('window:before:load', (win) => {
+  const RealResizeObserver = win.ResizeObserver;
+
+  let queueFlushTimeout;
+  let queue = [];
+
+  class ResizeObserverPolyfill {
+    constructor(callback) {
+      this.callback = callback;
+      this.observer = new RealResizeObserver(this.check.bind(this));
+    }
+
+    observe(element) {
+      this.observer.observe(element);
+    }
+
+    unobserve(element) {
+      this.observer.unobserve(element);
+    }
+
+    disconnect() {
+      this.observer.disconnect();
+    }
+
+    check(entries) {
+      queue = queue.filter((x) => x.cb !== this.callback);
+      queue.push({ cb: this.callback, args: entries });
+
+      if (!queueFlushTimeout) {
+        queueFlushTimeout = requestAnimationFrame(() => {
+          queueFlushTimeout = undefined;
+          const q = queue;
+          queue = [];
+          q.forEach(({ cb, args }) => cb(args));
+        }, 0);
+      }
+    }
+  }
+
   win.ResizeObserver = ResizeObserverPolyfill;
 });
