6755cf4c6971ceb08eead65765b709aae73bdaff
Shenoy Pratik <sgguruda@amazon.com>
2025-04-08 13:56:07 -0700

    Remove support for legacy notebooks (#2406)
    * remove support for legacy notebooks
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    * remove legacy endpoints and adaptors
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>
    
    ---------
    
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- server/adaptors/notebooks/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { DefaultBackend } from './default_backend';
-
-export const BACKEND: DefaultBackend = new DefaultBackend();
