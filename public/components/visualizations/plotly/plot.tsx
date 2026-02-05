1c501f0c5676f04fca76b66734e8bf8b29d5a65b
Adam Tackett <tackadam@amazon.com>
2026-02-05 15:03:24 -0800

    update test
    Signed-off-by: Adam Tackett <tackadam@amazon.com>

--- public/components/visualizations/plotly/plot.tsx
+++ public/components/visualizations/plotly/plot.tsx
@@ -4,7 +4,7 @@
  */
 
 import Plotly from 'plotly.js-dist';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import plotComponentFactory from 'react-plotly.js/factory';
 import { uiSettingsService } from '../../../../common/utils';
 
@@ -22,7 +22,14 @@ interface PltProps {
 }
 
 export function Plt(props: PltProps) {
+  const [isMounted, setIsMounted] = useState(false);
   const PlotComponent = plotComponentFactory(Plotly);
+
+  // Delay rendering until after mount to avoid getBoundingClientRect errors in React 18
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   const darkLayout = uiSettingsService.get('theme:darkMode')
     ? {
         paper_bgcolor: '#1D1E24',
@@ -72,6 +79,11 @@ export function Plt(props: PltProps) {
     displayModeBar: false,
     ...props.config,
   };
+
+  // Don't render Plotly until component is mounted to avoid React 18 timing issues
+  if (!isMounted) {
+    return <div />;
+  }
 
   return (
     <PlotComponent
