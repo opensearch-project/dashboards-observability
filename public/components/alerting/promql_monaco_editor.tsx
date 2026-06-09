bb6785892499efba348f2d015e117701f3d6c1ba
Riyaz Saxena <riysaxen@amazon.com>
2026-06-09 11:55:36 -0700

    fix: Correct opensearch_dashboards_react import path (5 levels up)
    Signed-off-by: Riyaz Saxena <riysaxen@amazon.com>

--- public/components/alerting/promql_monaco_editor.tsx
+++ public/components/alerting/promql_monaco_editor.tsx
@@ -15,7 +15,7 @@ import { monaco } from '@osd/monaco';
 import {
   CodeEditor,
   OpenSearchDashboardsContextProvider,
-} from '../../../../../../src/plugins/opensearch_dashboards_react/public';
+} from '../../../../../src/plugins/opensearch_dashboards_react/public';
 import { coreRefs } from '../../framework/core_refs';
 import { AlertingPromResourcesService } from './query_services/alerting_prom_resources_service';
 
