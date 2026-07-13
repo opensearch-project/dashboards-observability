b89f1f9d77cf0367d304732c4bc3290709b7103f
SuZhou-Joe <suzhou@amazon.com>
2026-07-14 02:04:20 +0800

    Adopt ESLint 10 / flat config (#2777)
    Migrate to flat eslint.config.js spreading @elastic/eslint-config-kibana + EUI (preserving local rules). Register cypress globals for .cypress specs (replacing the nested .cypress/.eslintrc.js). Closes #2776
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

---
+++ eslint.config.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const cypressPlugin = require('eslint-plugin-cypress');
+
+const osdConfig = require('@elastic/eslint-config-kibana');
+const { eui } = require('@elastic/eslint-config-kibana/extras');
+
+const LICENSE_HEADER = `/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */`;
+
+module.exports = [
+  // Replaces .eslintignore + package.json `eslintIgnore` (ESLint 10 reads neither).
+  {
+    ignores: [
+      'node_modules',
+      'data',
+      'build',
+      'target',
+      'cypress.config.js',
+      'common/query_manager/antlr/output/**',
+      '**/*.d.ts',
+    ],
+  },
+  ...osdConfig,
+  ...eui,
+  {
+    // Register Cypress + mocha globals for the .cypress/ integration specs
+    // (previously provided by the old kibana eslint config's cypress env).
+    files: ['.cypress/**/*.{js,ts}', 'cypress.config.js'],
+    plugins: {
+      cypress: cypressPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...cypressPlugin.configs.globals.languageOptions.globals,
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 0,
+      '@osd/eslint/no-restricted-paths': [
+        'error',
+        {
+          basePath: __dirname,
+          zones: [
+            {
+              target: ['(public|server)/**/*'],
+              from: ['../../packages/**/*', 'packages/**/*'],
+            },
+          ],
+        },
+      ],
+      'jest/expect-expect': [
+        'warn',
+        {
+          // Allow using custom expect test helpers as long as the name starts with `expect`.
+          assertFunctionNames: ['expect*'],
+        },
+      ],
+      '@osd/eslint/require-license-header': ['error', { licenses: [LICENSE_HEADER] }],
+    },
+  },
+];
